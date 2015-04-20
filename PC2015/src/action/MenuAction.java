4db6e6c229a4952ce0a82cf586f5437f642a3fc0
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-20 03:52:58 +0000

    MenuActionのjavaファイルを変更しました。

--- PC2015/src/action/MenuAction.java
+++ PC2015/src/action/MenuAction.java
@@ -7,7 +7,15 @@ import org.apache.struts2.dispatcher.ServletRedirectResult;
 
 @Results({
 	@Result(name = "main1", value = "main1.action", type = ServletRedirectResult.class),
-	@Result(name = "main2", value = "main2.action", type = ServletRedirectResult.class)
+	@Result(name = "main2", value = "main2.action", type = ServletRedirectResult.class),
+	@Result(name = "main3", value = "main3.action", type = ServletRedirectResult.class),
+	@Result(name = "main4", value = "main4.action", type = ServletRedirectResult.class),
+	@Result(name = "main5", value = "main5.action", type = ServletRedirectResult.class),
+	@Result(name = "main6", value = "main6.action", type = ServletRedirectResult.class),
+	@Result(name = "main7", value = "main7.action", type = ServletRedirectResult.class),
+	@Result(name = "main8", value = "main8.action", type = ServletRedirectResult.class),
+	@Result(name = "main9", value = "main9.action", type = ServletRedirectResult.class),
+	@Result(name = "main10", value = "main10.action", type = ServletRedirectResult.class)
 })
 
 public class MenuAction extends AbstractAction {
@@ -24,5 +32,37 @@ public class MenuAction extends AbstractAction {
 
 	public String gotoMain2() {
 		return "main2";
+	}
+	
+	public String gotoMain3() {
+		return "main3";
+	}
+
+	public String gotoMain4() {
+		return "main4";
+	}
+	
+	public String gotoMain5() {
+		return "main5";
+	}
+
+	public String gotoMain6() {
+		return "main6";
+	}
+	
+	public String gotoMain7() {
+		return "main7";
+	}
+
+	public String gotoMain8() {
+		return "main8";
+	}
+	
+	public String gotoMain9() {
+		return "main9";
+	}
+
+	public String gotoMain10() {
+		return "main10";
 	}
 }
