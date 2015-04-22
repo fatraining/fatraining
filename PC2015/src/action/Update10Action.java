ea834a5928432893ccc2079cc49c8945ab50ef74
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-22 23:53:28 +0000

    4/23更新UpdateAction横山

--- PC2015/src/action/Update10Action.java
+++ PC2015/src/action/Update10Action.java
@@ -10,6 +10,9 @@ import org.apache.struts2.dispatcher.ServletRedirectResult;
 import org.hibernate.HibernateException;
 import org.hibernate.classic.Session;
 
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 import controller.TaskUtil;
 
 @Result(name = "main10", value = "main10.action", type = ServletRedirectResult.class)
@@ -19,8 +22,8 @@ public class Update10Action extends AbstractAction {
 
 	public String eatFood;
 	public String eatCalory;
-	public String eat_year ;
-	public String eat_month ;
+	public String eat_year;
+	public String eat_month;
 	public String eat_day;
 	public String eat_hour;
 	public String entry_day;
@@ -32,71 +35,78 @@ public class Update10Action extends AbstractAction {
 	public String errormsg;
 
 	public String execute() throws Exception {
-		this.update_id = (String)this.sessionMap.get("update_id");
-
-
+		this.update_id = (String) this.sessionMap.get("update_id");
 
 		return "success";
 	}
 
-	public String insert(){
+	public String insert() {
+			//日付の設定
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd k:m:s");
+		entry_day = String.valueOf(sdf.format(date));
+		renew_day = String.valueOf(sdf.format(date));
+			
 		Session session = TaskUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
 
 		IDofEat insert_id_table = new IDofEat();
 		DetailEat insert_detail_table = new DetailEat();
 
-//		insert_detail_table.setEat_year(checkcode(this.eat_year));
-//		insert_detail_table.setEat_month(checkcode(this.eat_month));
-//		insert_detail_table.setEat_day(checkcode(this.eat_day));
-//		insert_detail_table.setEat_hour(checkcode(this.eat_hour));
-//		insert_id_table.setEatFood(checkcode(this.eatFood));
-//		insert_id_table.setEatCalory(checkcode(this.eatCalory));
-//		insert_detail_table.setEntry_day(checkcode(this.entry_day));
-//		insert_detail_table.setRenew_day(checkcode(this.renew_day));
-//		insert_detail_table.setEntry_day(checkcode(this.entry_userid));
-//		insert_detail_table.setRenew_day(checkcode(this.renew_userid));
-//		insert_detail_table.setRenew_flg(checkcode(this.renew_flg));
-//		insert_detail_table.setDeleteflg(checkcode(this.delete_flg));
-		
-		String[] data = {this.eat_year,this.eat_month,this.eat_day,this.eat_hour,this.eatFood, this.eatCalory};
-		int i=0;
-		for(String temp : data){
-			if(temp.length()>50){
-				this.errormsg = "50文字以下で入力してください";
-				return "error";
-			}
-			if(temp.length()<1)i++;
-			if(i>14){
-				this.errormsg = "未入力は登録できません";
-				return "error";
-			}
+		insert_detail_table.setEat_year(this.eat_year);
+		insert_detail_table.setEat_month(this.eat_month);
+		insert_detail_table.setEat_day(this.eat_day);
+		insert_detail_table.setEat_hour(this.eat_hour);
+		insert_id_table.setEatFood(this.eatFood);
+		insert_id_table.setEatCalory(this.eatCalory);
+		insert_id_table.setEntry_day(this.entry_day);
+		insert_id_table.setRenew_day(this.renew_day);
+		// insert_detail_table.setEntry_day(checkcode(this.entry_userid));
+		// insert_detail_table.setRenew_day(checkcode(this.renew_userid));
+		// insert_detail_table.setRenew_flg(checkcode(this.renew_flg));
+		// insert_detail_table.setDeleteflg(checkcode(this.delete_flg));
 
-			try{
-				session.save(insert_detail_table);
-				session.save(insert_id_table);
-			}catch(HibernateException e){
-				e.printStackTrace();
-				session.getTransaction().rollback();
-			}
-			
+		// String[] data = {this.eat_year, this.eat_month, this.eat_day,
+		// this.eat_hour, this.eatFood, this.eatCalory};
+		// int i=0;
+		// for(String temp : data){
+		// if(temp.length()>50){
+		// this.errormsg = "50文字以下で入力してください";
+		// return "error";s
+		// }
+		// if(temp.length()<1)i++;
+		// if(i>14){
+		// this.errormsg = "未入力は登録できません";
+		// return "error";
+		// }
+
+		try {
+			session.save(insert_detail_table);
+			session.save(insert_id_table);
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
 		}
-		
+
+		// }
+
 		session.getTransaction().commit();
 		return "main10";
 
 	}
-	
+
 	public String delete() {
-		this.update_id = (String)this.sessionMap.get("update_id");
-		if(this.update_id.isEmpty()){
+		this.update_id = (String) this.sessionMap.get("update_id");
+		if (this.update_id.isEmpty()) {
 			return "main10";
 		}
 		Session session = TaskUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
 		try {
-			DetailEat detaileat = (DetailEat)session.load(DetailEat.class, Integer.valueOf(update_id));
-			IDofEat idofeat = (IDofEat) session.load(IDofEat.class,  Integer.valueOf(update_id));
+			DetailEat detaileat = (DetailEat) session.load(DetailEat.class,
+					Integer.valueOf(update_id));
+			IDofEat idofeat = (IDofEat) session.load(IDofEat.class,
+					Integer.valueOf(update_id));
 			session.delete(detaileat);
 			session.delete(idofeat);
 		} catch (HibernateException e) {
@@ -106,25 +116,25 @@ public class Update10Action extends AbstractAction {
 		session.getTransaction().commit();
 		return "main10";
 	}
-//	public String checkcode(String code) {
-//
-//
-//		//code = code.replaceAll("[^a-zA-Z_0-9]","_");
-//		if(!code.matches("[a-zA-Z_0-9]{0,50}")){
-//			//return "";
-//		}
-//		return code;
-//	}
-//	private static boolean checkCharacterCode(String str, String encoding) {
-//		if (str == null) {
-//			return true;
-//		}
-//
-//		try {
-//			byte[] bytes = str.getBytes(encoding);
-//			return str.equals(new String(bytes, encoding));
-//		} catch (UnsupportedEncodingException ex) {
-//			throw new RuntimeException("エンコード名称が正しくありません。", ex);
-//		}
-//	}
+
+	// public String checkcode(String code) {
+
+	// code = code.replaceAll("[^a-zA-Z_0-9]","_");
+	// if(!code.matches("[a-zA-Z_0-9]{0,50}")){
+	// return "";
+	// }
+	// return code;
+	// }
+	private static boolean checkCharacterCode(String str, String encoding) {
+		if (str == null) {
+			return true;
+		}
+
+		try {
+			byte[] bytes = str.getBytes(encoding);
+			return str.equals(new String(bytes, encoding));
+		} catch (UnsupportedEncodingException ex) {
+			throw new RuntimeException("エンコード名称が正しくありません。", ex);
+		}
+	}
 }
