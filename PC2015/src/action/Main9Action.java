704fc6130aa783a9676acc0ebba1fd6bb99a9046
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-22 01:16:03 +0000

    無記入のとき検索結果は全部出ますが、条件を入れると検索結果は出ません。

---
+++ PC2015/src/action/Main9Action.java
@@ -0,0 +1,97 @@
+package action;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import controller.LiofTaManager;
+import controller.Result9Manager;
+import action.AbstractAction;
+import model.Result9Table;
+
+public class Main9Action extends AbstractAction {
+
+	private static final long serialVersionUID = 1L;
+
+	public String name;
+	public String food;
+	public String drink;
+	public String search;
+	public String result;
+	public String update_id;
+	public String delete;
+	public String do_print;
+
+	private Result9Manager linkController;
+	private LiofTaManager allController;
+	public ArrayList<Result9Table> outputTable;
+
+
+	private String getDefaultName() {
+		this.name = "";
+		return this.name;
+	}
+
+	private String getDefaultFood() {
+		this.food = "";
+		return this.food;
+	}
+
+	private String getDefaultDrink() {
+		this.drink = "";
+		return this.drink;
+	}
+
+	@Override
+	public String execute() {
+		this.name = getDefaultName();
+		this.food = getDefaultFood();
+		this.drink = getDefaultDrink();
+		this.delete = "faluse";
+		return "success";
+	}
+
+	public String reset() {
+		this.name = getDefaultName();
+		this.food = getDefaultFood();
+		this.drink = getDefaultDrink();
+		return "success";
+	}
+
+	public String search() {
+		this.search = "true";
+		if (this.name.isEmpty() && this.food.isEmpty() && this.drink.isEmpty()) {
+			try {
+				printall();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			linkController = new Result9Manager();
+			this.outputTable = linkController.resultList(this.name, this.food,
+					this.drink);
+		}
+		this.delete = "true";
+		return "success";
+	}
+
+	public String printall() {
+		this.do_print = "true";
+		allController = new LiofTaManager();
+		this.outputTable = allController.resultList(this.name, this.food,
+				this.drink);
+		this.delete = "true";
+		return "success";
+	}
+
+	public String update() {
+		this.sessionMap.put("update_id", this.update_id);
+		try {
+			this.response.sendRedirect("/PC2015/update9.action");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return "success";
+	}
+
+}
