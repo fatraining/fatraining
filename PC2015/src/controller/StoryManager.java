2a4c6692ee4970f9d98377037e794ba512a5777f
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-23 07:13:46 +0000

    StoryManagerをコミットしました。

---
+++ PC2015/src/controller/StoryManager.java
@@ -0,0 +1,66 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Story;
+import model.Genre;
+import model.Result3Table;
+
+import org.hibernate.classic.Session;
+
+public class StoryManager extends TaskUtil {
+
+	public  List<?> resultTable;
+	public  ArrayList<Result3Table> outputTable;
+
+	public ArrayList<Result3Table> resultList() {
+		Session session = TaskUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+
+		String select = "SELECT * FROM story d,tb_genre i";
+		String where1 = "WHERE d.genre_id = i.id";
+		String sql    = select + " "+where1;
+
+		try {
+			resultTable = session.createSQLQuery(sql)
+					.addEntity("Story", Story.class)
+					.addEntity("Genre", Genre.class).list();
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		}
+		session.getTransaction().commit();
+
+		this.outputTable = tableTrans(resultTable);
+
+		return outputTable;
+	}
+	public ArrayList<Result3Table> tableTrans(List<?> resultTable){
+		ArrayList<Result3Table> tempTable = new ArrayList<Result3Table>();
+		Object[] obj;
+		try {
+			for(int i = 0 ; i < resultTable.size() ; i++){
+				Result3Table temp = new Result3Table();
+				obj = (Object[]) resultTable.get(i);
+				Story story =  (Story)obj[0];
+				Genre genre     = (Genre)obj[1];
+				temp.setGenre(genre.getGenre());
+				temp.setId(story.getId());
+				temp.setTitle(story.getTitle());
+//				temp.setSignup(story.getSignup());
+//				temp.setUPdate(story.getUpdate());
+//				temp.setSignupUSERID(story.getSignupUSERID());
+//				temp.setUpdateUSERID(story.getUpdateUSERID());
+//				temp.setExclusiveFLG(story.getExclusiveFLG());
+//				temp.setDeleteFLG(story.getDeleteFLG());
+				tempTable.add(temp);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+
+		return tempTable;
+	}
+}
