f8bd574d36bd19fec58bf2d6c64ff163d9e5c52d
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-23 05:43:29 +0000

    Update7をコミットします。

---
+++ PC2015/src/action/Update7Action.java
@@ -0,0 +1,119 @@
+package action;
+
+import java.io.UnsupportedEncodingException;
+
+import model.Movie;
+import model.MovieGenre;
+
+import org.apache.struts2.config.Result;
+import org.apache.struts2.dispatcher.ServletRedirectResult;
+import org.hibernate.HibernateException;
+import org.hibernate.classic.Session;
+
+import controller.TaskUtil;
+
+@Result(name = "main7", value = "main7.action", type = ServletRedirectResult.class)
+public class Update7Action extends AbstractAction {
+	private static final long serialVersionUID = 1L;
+	public String update_id;
+	public String title;
+	public String genreId;
+	public String exhibition_year;
+//	public String registration_date ;
+//	public String renewal_date ;
+//	public String registration_userid;
+//	public String renewal_userid;
+//	public int control;
+//	public int delete;
+	public String errormsg;
+
+	public String execute() throws Exception {
+		this.update_id = (String)this.sessionMap.get("update_id");
+
+
+
+		return "success";
+	}
+
+	public String insert(){
+		Session session = TaskUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+
+		Movie insert_movie_table = new Movie();
+//		MovieGenre insert_movie_genre_table = new MovieGenre();
+		insert_movie_table.setTitle(this.title);
+		insert_movie_table.setGenreId(this.genreId);
+		insert_movie_table.setExhibition_year(this.exhibition_year);
+//		insert_movie_table.setRegistration_date(this.registration_date);
+//		insert_movie_table.setRenewal_date(this.renewal_date);
+//		insert_movie_table.setRegistration_userid(this.registration_userid);
+//		insert_movie_table.setRenewal_userid(this.renewal_userid);
+//		insert_movie_table.setControl(this.control);
+//		String[] data = {this.title,this.genre,this.exhibition_year};
+//		int i=0;
+//		for(String temp : data){
+//			if(temp.length()>50){
+//				this.errormsg = "50文字以下で入力してください";
+//				return "error";
+//			}
+//			if(temp.length()<1)i++;
+//			if(i>3){
+//				this.errormsg = "未入力は登録できません";
+//				return "error";
+//			}
+
+			try{
+				session.save(insert_movie_table);
+//				session.save(insert_movie_genre_table);
+
+			}catch(HibernateException e){
+				e.printStackTrace();
+				session.getTransaction().rollback();
+			}
+
+//		}
+		session.getTransaction().commit();
+		return "main7";
+
+	}
+	public String delete() {
+		this.update_id = (String)this.sessionMap.get("update_id");
+		if(this.update_id.isEmpty()){
+			return "main7";
+		}
+		Session session = TaskUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		try {
+			Movie movie = (Movie)session.load(Movie.class,update_id);
+			MovieGenre moviegenre = (MovieGenre) session.load(MovieGenre.class,update_id);
+			session.delete(movie);
+			session.delete(moviegenre);
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		}
+		session.getTransaction().commit();
+		return "main7";
+	}
+//	public String checkcode(String code) {
+//
+//
+//		//code = code.replaceAll("[^a-zA-Z_0-9]","_");
+//		if(!code.matches("[a-zA-Z_0-9]{0,50}")){
+//			//return "";
+//		}
+//		return code;
+//	}
+//	private static boolean checkCharacterCode(String str, String encoding) {
+//		if (str == null) {
+//			return true;
+//		}
+//
+//		try {
+//			byte[] bytes = str.getBytes(encoding);
+//			return str.equals(new String(bytes, encoding));
+//		} catch (UnsupportedEncodingException ex) {
+//			throw new RuntimeException("エンコード名称が正しくありません。", ex);
+//		}
+//	}
+}
