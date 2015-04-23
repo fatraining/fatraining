5f6cf4040700f962b73c96dfc78d1c5bcbad76f7
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-23 07:07:13 +0000

    ResultTable6。javaをコミットします。篠崎

---
+++ PC2015/src/model/ResultTable6.java
@@ -0,0 +1,166 @@
+package model;
+
+import java.io.Serializable;
+
+public class ResultTable6 implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private int id;
+	private String title;
+	private int series;
+	private String user;
+	private String upDay;
+	private String userId;
+	private String upUser;
+	private String nonStyle;
+	private String del;
+	private int i;
+	private String se;
+	private String u;
+	private String upD;
+	private String userI;
+	private String upU;
+	private String nonS;
+	private String d;
+
+	public ResultTable6() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getSeries() {
+		return series;
+	}
+
+	public void setSeries(int series) {
+		this.series = series;
+	}
+
+	public String setUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getUpDay() {
+		return upDay;
+	}
+
+	public void setUpDay(String upDay) {
+		this.upDay = upDay;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUpUser() {
+		return upUser;
+	}
+
+	public void setUpUser(String upUser) {
+		this.upUser = upUser;
+	}
+
+	public String getNonStyle() {
+		return nonStyle;
+	}
+
+	public void setNonStyle(String nonStyle) {
+		this.nonStyle = nonStyle;
+	}
+
+	public String getDel() {
+		return del;
+	}
+
+	public void setDel(String del) {
+		this.del = del;
+	}
+
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+	
+	public String getSe() {
+		return se;
+	}
+
+	public void setSe(String se) {
+		this.se = se;
+	}
+
+	public String getU() {
+		return u;
+	}
+
+	public void setU(String u) {
+		this.u = u;
+	}
+
+	public String upD() {
+		return upD;
+	}
+
+	public void setUpD(String upD) {
+		this.upD = upD;
+	}
+
+	public String getUserI() {
+		return userI;
+	}
+
+	public void setUserI(String userI) {
+		this.userI = userI;
+	}
+
+	public String upU() {
+		return upU;
+	}
+
+	public void setUpU(String upU) {
+		this.upU = upU;
+	}
+
+	public String getNonS() {
+		return nonS;
+	}
+
+	public void setNonS(String nonS) {
+		this.nonS = nonS;
+	}
+
+	public String getD() {
+		return d;
+	}
+
+	public void setD(String d) {
+		this.d = d;
+	}
+
+}
