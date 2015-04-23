d4c556bb1064caf1a0b4b1b53fa32dfa9abbc7b3
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-23 06:30:20 +0000

    武政

---
+++ PC2015/src/model/Result1Table.java
@@ -0,0 +1,120 @@
+package model;
+
+import java.io.Serializable;
+
+public class Result1Table implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private int id;
+	private String name;
+	private String personality;
+	private String home;
+	private int birthday;
+	private String hobby;
+	private String day;
+	private String new_day;
+//	private String userid;
+//	private String new_userid;
+//	private int time_stamp;
+//	private int delete;
+
+	public Result1Table() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPersonality() {
+		return personality;
+	}
+
+	public void setPersonality(String personality) {
+		this.personality = personality;
+	}
+
+	public String getHome() {
+		return home;
+	}
+
+	public void setHome(String home) {
+		this.home = home;
+	}
+
+	public int getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(int birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getHobby() {
+		return hobby;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public String getNew_day() {
+		return new_day;
+	}
+
+	public void setNew_day(String new_day) {
+		this.new_day = new_day;
+	}
+//
+//	public String getUserid() {
+//		return userid;
+//	}
+//
+//	public void setgetUserid(String userid) {
+//		this.userid = userid;
+//	}
+//
+//	public String getNew_userid() {
+//		return new_userid;
+//	}
+//
+//	public void setgetNew_userid(String new_userid) {
+//		this.new_userid = new_userid;
+//	}
+//
+//	public int getTime_stamp() {
+//		return time_stamp;
+//	}
+//
+//	public void setTime_stamp(int time_stamp) {
+//		this.time_stamp = time_stamp;
+//	}
+//
+//	public int getDelete() {
+//		return delete;
+//	}
+//
+//	public void setDelete(int delete) {
+//		this.delete = delete;
+//	}
+}
