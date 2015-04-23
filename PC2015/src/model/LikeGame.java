8239d4a53d509c77635def6dba162c5c702e5cbf
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-23 07:05:50 +0000

    LikeGame.javaをコミットします。篠崎

---
+++ PC2015/src/model/LikeGame.java
@@ -0,0 +1,95 @@
+package model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="like_game")
+public class LikeGame implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private int id;
+	private String title ;
+	private int series ;
+	private String user;
+	private String upDay;
+	private String userId;
+	private String upUser;
+	private String nonStyle ;
+	private String del ;
+	
+	/*--------------------------------------------------*/
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	public int getId() {
+		return id;
+	}
+	@Column(name="title")
+	public String getTitle() {
+		return title;
+	}
+	@Column(name="series")
+	public int getSeries() {
+		return series;
+	}
+	@Column(name="user")
+	public String getUser() {
+		return user;
+	}
+	@Column(name="upDay")
+	public String getUpDay() {
+		return upDay;
+	}
+	@Column(name="userId")
+	public String getUserId() {
+		return userId;
+	}
+	@Column(name="upUser")
+	public String getUpUser() {
+		return upUser;
+	}
+	@Column(name="nonStyle")
+	public String getNonStyle() {
+		return nonStyle;
+	}	
+	@Column(name="del")
+	public String getDel() {
+		return del;
+	}
+/*--------------------------------------------------*/
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setSeries(int series) {
+		this.series = series;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public void setUpDay(String upDay) {
+		this.upDay = upDay;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public void setUpUser(String upUser) {
+		this.upUser = upUser;
+	}
+	public void setNonStyle(String nonStyle) {
+		this.nonStyle = nonStyle;
+	}
+	public void setDel(String del) {
+		this.del = del;
+	}
+}
