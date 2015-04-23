30be14e4d518ab625726dac1b3d8224b66267ebf
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-23 07:15:04 +0000

    大野

---
+++ PC2015/src/model/User_Character.java
@@ -0,0 +1,111 @@
+package model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "User_Character")
+public class User_Character implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private int id;
+	private String personality;
+	private String interest;
+	private String day;
+	private String newday;
+	private String userid;
+	private String newuserid;
+	private int delete;
+	private int flg;
+
+	/*--------------------------------------------------*/
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	public int getId() {
+		return id;
+	}
+
+	@Column(name = "personality")
+	public String getPersonality() {
+		return personality;
+	}
+
+	@Column(name = "interest")
+	public String getInterest() {
+		return interest;
+	}
+
+	@Column(name = "day")
+	public String getDay() {
+		return day;
+	}
+
+	@Column(name = "newday")
+	public String getNewday() {
+		return newday;
+	}
+
+	@Column(name = "userid")
+	public String getUserid() {
+		return userid;
+	}
+
+	@Column(name = "newuserid")
+	public String getNewuserid() {
+		return userid;
+	}
+
+	@Column(name = "delete")
+	public int getDelete() {
+		return delete;
+	}
+
+	@Column(name = "flg")
+	public int getFlg() {
+		return flg;
+	}
+
+	/*--------------------------------------------------*/
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setPersonality(String personality) {
+		this.personality = personality;
+	}
+
+	public void setInterest(String interest) {
+		this.interest = interest;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public void setNewday(String newday) {
+		this.newday = newday;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public void setNewuserid(String newuserid) {
+		this.newuserid = newuserid;
+	}
+
+	public void setDelete(int delete) {
+		this.delete = delete;
+	}
+
+	public void setFlg(int flg) {
+		this.flg = flg;
+	}
+}
