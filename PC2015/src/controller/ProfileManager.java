e3537dc406a5fadacd14b046aeda758af98f48bd
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-23 06:29:26 +0000

    武政

---
+++ PC2015/src/controller/ProfileManager.java
@@ -0,0 +1,66 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.My_hobby;
+import model.Profile;
+import model.Result1Table;
+
+import org.hibernate.classic.Session;
+
+public class ProfileManager extends TaskUtil {
+
+	public List<?> resultTable;
+	public ArrayList<Result1Table> outputTable;
+
+	public ArrayList<Result1Table> resultList() {
+		Session session = TaskUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+
+		String select = "SELECT * FROM table_profile i, table_hobby d";
+		String where1 = "WHERE i.id = d.id";
+		String sql = select + " " + where1;
+
+		try {
+			resultTable = session.createSQLQuery(sql)
+					.addEntity("table_hobby", My_hobby.class)
+					.addEntity("table_profile", Profile.class).list();
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		}
+		session.getTransaction().commit();
+
+		this.outputTable = tableTrans(resultTable);
+
+		return outputTable;
+	}
+
+	public ArrayList<Result1Table> tableTrans(List<?> resultTable) {
+		ArrayList<Result1Table> tempTable = new ArrayList<Result1Table>();
+		Object[] obj;
+		try {
+			for (int i = 0; i < resultTable.size(); i++) {
+				Result1Table temp = new Result1Table();
+				obj = (Object[]) resultTable.get(i);
+				My_hobby my_hobby = (My_hobby) obj[0];
+				Profile profile = (Profile) obj[1];
+				temp.setId(profile.getId());
+				temp.setName(profile.getName());
+				temp.setPersonality(profile.getPersonality());
+				temp.setHome(profile.getHome());
+				temp.setBirthday(profile.getBirthday());
+				temp.setDay(profile.getDay());
+				temp.setNew_day(profile.getNew_day());
+				temp.setHobby(my_hobby.getHobby());
+				tempTable.add(temp);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+
+		return tempTable;
+	}
+}
