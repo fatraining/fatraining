72d247584fbb47c8ec87790dd7ec1cae2f26f0aa
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-23 05:40:17 +0000

    江口

--- PC2015/src/controller/Result9Manager.java
+++ PC2015/src/controller/Result9Manager.java
@@ -10,7 +10,7 @@ import model.CoofTa;
 import model.Result9Table;
 
 public class Result9Manager extends TaskUtil {
-	public List<?> resultTable;
+	public List<?> result9Table;
 	public ArrayList<Result9Table> outputTable;
 
 	public ArrayList<Result9Table> resultList(String name, String food,
@@ -24,37 +24,37 @@ public class Result9Manager extends TaskUtil {
 				food = "%";
 			if (drink.isEmpty())
 				drink = "%";
-			String select = "SELECT * FROM table_color d, table_like i";
-			String where1 = "WHERE d.id=i.color";
-//			String where2 = "AND (i.name LIKE '" + name + "' AND i.food LIKE '"
-//					+ food + "' AND i.drink LIKE '" + drink + "')";
-			String sql = select + " " + where1/* + " " + where2*/;
-			resultTable = session.createSQLQuery(sql)
-					.addEntity("CoofTa", CoofTa.class)
-					.addEntity("LiofTa", LiofTa.class).list();
+			String select = "SELECT * FROM table_like d, table_color i";
+			String where1 = "WHERE d.id=i.id";
+			String where2 = "AND (d.name LIKE '" + name + "' AND d.food LIKE '"
+					+ food + "' AND d.drink LIKE '" + drink + "')";
+			String sql = select + " " + where1 + " " + where2;
+			result9Table = session.createSQLQuery(sql)
+					.addEntity("LiofTa", LiofTa.class)
+					.addEntity("CoofTa", CoofTa.class).list();
 		} catch (Exception e) {
 			e.printStackTrace();
 			session.getTransaction().rollback();
 		}
 		session.getTransaction().commit();
-		this.outputTable = tableTrans(resultTable);
+		this.outputTable = tableTrans(result9Table);
 		return outputTable;
 	}
 
-	public ArrayList<Result9Table> tableTrans(List<?> resultTable) {
+	public ArrayList<Result9Table> tableTrans(List<?> result9Table) {
 		ArrayList<Result9Table> tempTable = new ArrayList<Result9Table>();
 		Object[] obj;
 		try {
-			for (int i = 0; i < resultTable.size(); i++) {
+			for (int i = 0; i < result9Table.size(); i++) {
 				Result9Table temp = new Result9Table();
-				obj = (Object[]) resultTable.get(i);
-				CoofTa coofta = (CoofTa) obj[0];
-				LiofTa liofta = (LiofTa) obj[1];
+				obj = (Object[]) result9Table.get(i);
+				LiofTa liofta = (LiofTa) obj[0];
+				CoofTa coofta = (CoofTa) obj[1];
 				temp.setId(liofta.getId());
 				temp.setName(liofta.getName());
 				temp.setFood(liofta.getFood());
 				temp.setDrink(liofta.getDrink());
-				temp.setColor(coofta.getColorNm());
+				temp.setColorNm(coofta.getColorNm());
 				temp.setTaste(coofta.getTaste());
 				// temp.setDate_of_registration(coofta.getDate_of_registration());
 				// temp.setUpdate_date(coofta.getUpdate_date());
