1292b3bcd61186cff788cc97b7035b060766e6a2
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-23 06:47:02 +0000

    江口

---
+++ PC2015/src/model/Result9Table.java
@@ -0,0 +1,118 @@
+package model;
+
+import java.io.Serializable;
+
+public class Result9Table implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private int id;
+	private String name;
+	private String food;
+	private String drink;
+	private String colorNm;
+	private String taste;
+//	private String date_of_registration;
+//	private String update_date;
+//	private String registration_userid;
+//	private String update_userid;
+//	private int time_stamp;
+//	private int delete;
+
+	public Result9Table() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFood() {
+		return food;
+	}
+
+	public void setFood(String food) {
+		this.food = food;
+	}
+
+	public String getDrink() {
+		return drink;
+	}
+
+	public void setDrink(String drink) {
+		this.drink = drink;
+	}
+
+	public String getColorNm() {
+		return colorNm;
+	}
+
+	public void setColorNm(String colorNm) {
+		this.colorNm = colorNm;
+	}
+
+	public String getTaste() {
+		return taste;
+	}
+
+	public void setTaste(String taste) {
+		this.taste = taste;
+	}
+
+//	public String getDate_of_registration() {
+//		return date_of_registration;
+//	}
+//
+//	public void setDate_of_registration(String date_of_registration) {
+//		this.date_of_registration = date_of_registration;
+//	}
+//
+//	public String getUpdate_date() {
+//		return update_date;
+//	}
+//
+//	public void setUpdate_date(String update_date) {
+//		this.update_date = update_date;
+//	}
+//
+//	public String getRegistration_userid() {
+//		return registration_userid;
+//	}
+//
+//	public void setRegistration_userid(String registration_userid) {
+//		this.registration_userid = registration_userid;
+//	}
+//
+//	public String getUpdate_userid() {
+//		return update_userid;
+//	}
+//
+//	public void setUpdate_userid(String update_userid) {
+//		this.update_userid = update_userid;
+//	}
+//
+//	public int getTime_stamp() {
+//		return time_stamp;
+//	}
+//
+//	public void setTime_stamp(int time_stamp) {
+//		this.time_stamp = time_stamp;
+//	}
+
+//	public int getDelete() {
+//		return delete;
+//	}
+//
+//	public void setDelete(int delete) {
+//		this.delete = delete;
+//	}
+}
