98917e17475720da4ac314ac4c7e5963a0b35b90
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-23 06:14:57 +0000

    menu2をコミットしました。

---
+++ PC2015/src/menu2.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 2015 年 4 朁E23 日 08:06
+-- サーバのバージョン： 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `task_master`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `genre`
+--
+
+CREATE TABLE IF NOT EXISTS `genre` (
+`id` int(50) NOT NULL,
+  `genreNm` char(50) DEFAULT NULL,
+  `record_date` char(50) DEFAULT NULL,
+  `reset_date` char(50) DEFAULT NULL,
+  `entry_userId` char(50) DEFAULT NULL,
+  `record_userId` char(50) DEFAULT NULL,
+  `exclusive_FLG` int(50) DEFAULT NULL,
+  `delete_FLG` int(50) DEFAULT NULL,
+  `genre` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `genre`
+--
+
+INSERT INTO `genre` (`id`, `genreNm`, `record_date`, `reset_date`, `entry_userId`, `record_userId`, `exclusive_FLG`, `delete_FLG`, `genre`) VALUES
+(1, '焼き菓子', NULL, NULL, NULL, NULL, 0, 0, NULL),
+(2, '冷菓', NULL, NULL, NULL, NULL, 0, 0, NULL),
+(3, 'チョコレート菓子', NULL, NULL, NULL, NULL, 0, 0, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `sweets`
+--
+
+CREATE TABLE IF NOT EXISTS `sweets` (
+`id` int(50) NOT NULL,
+  `name` char(50) DEFAULT NULL,
+  `genre_id` int(50) DEFAULT NULL,
+  `record_date` char(50) DEFAULT NULL,
+  `reset_date` char(50) DEFAULT NULL,
+  `entry_userId` char(50) DEFAULT NULL,
+  `record_userId` char(50) DEFAULT NULL,
+  `exclusive_FLG` int(50) DEFAULT NULL,
+  `delete_FLG` int(50) DEFAULT NULL,
+  `genre` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `sweets`
+--
+
+INSERT INTO `sweets` (`id`, `name`, `genre_id`, `record_date`, `reset_date`, `entry_userId`, `record_userId`, `exclusive_FLG`, `delete_FLG`, `genre`) VALUES
+(1, 'クッキー', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(2, 'トリュフ', 3, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(3, 'プリン', 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(4, 'スコーン', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(5, 'ババロア', 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(6, '羊羹', 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(7, 'バウムクーヘン', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(8, 'ゼリー', 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(9, 'ガトーショコラ', 3, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `genre`
+--
+ALTER TABLE `genre`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sweets`
+--
+ALTER TABLE `sweets`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `genre`
+--
+ALTER TABLE `genre`
+MODIFY `id` int(50) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+--
+-- AUTO_INCREMENT for table `sweets`
+--
+ALTER TABLE `sweets`
+MODIFY `id` int(50) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
