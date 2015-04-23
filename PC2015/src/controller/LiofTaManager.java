4c0b37c3c30b2952843f7a9f6102b50b1b8b104d
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-23 05:40:06 +0000

    江口

--- PC2015/src/controller/LiofTaManager.java
+++ PC2015/src/controller/LiofTaManager.java
@@ -3,60 +3,60 @@ package controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import model.CoofTa;
 import model.LiofTa;
+import model.CoofTa;
 import model.Result9Table;
 
 import org.hibernate.classic.Session;
 
-public class LiofTaManager extends TaskUtil{
-	public List<?> resultTable;
+public class LiofTaManager extends TaskUtil {
+	public List<?> result9Table;
 	public ArrayList<Result9Table> outputTable;
 
-	public ArrayList<Result9Table> resultList(String name, String food, String drink) {
+	public ArrayList<Result9Table> resultList() {
 		Session session = TaskUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
 
-		String select = "SELECT * FROM table_color d,table_like i";
-		String where1 = "WHERE d.id=i.color";
+		String select = "SELECT * FROM table_like d,table_color i";
+		String where1 = "WHERE d.id=i.id";
 		String sql = select + " " + where1;
 
 		try {
-			resultTable = session.createSQLQuery(sql)
-					.addEntity("table_color", CoofTa.class)
-					.addEntity("table_like", LiofTa.class).list();
+			result9Table = session.createSQLQuery(sql)
+					.addEntity("table_like", LiofTa.class)
+					.addEntity("table_color", CoofTa.class).list();
 		} catch (Exception e) {
 			e.printStackTrace();
 			session.getTransaction().rollback();
 		}
 		session.getTransaction().commit();
 
-		this.outputTable = tableTrans(resultTable);
+		this.outputTable = tableTrans(result9Table);
 
 		return outputTable;
 	}
 
-	public ArrayList<Result9Table> tableTrans(List<?> resultTable) {
+	public ArrayList<Result9Table> tableTrans(List<?> result9Table) {
 		ArrayList<Result9Table> tempTable = new ArrayList<Result9Table>();
 		Object[] obj;
 		try {
-			for (int i = 0; i < resultTable.size(); i++) {
+			for (int i = 0; i < result9Table.size(); i++) {
 				Result9Table temp = new Result9Table();
-				obj = (Object[]) resultTable.get(i);
-				CoofTa coofta = (CoofTa) obj[0];
-				LiofTa liofta = (LiofTa) obj[1];
+				obj = (Object[]) result9Table.get(i);
+				LiofTa liofta = (LiofTa) obj[0];
+				CoofTa coofta = (CoofTa) obj[1];
 				temp.setId(liofta.getId());
 				temp.setName(liofta.getName());
 				temp.setFood(liofta.getFood());
 				temp.setDrink(liofta.getDrink());
-				temp.setColor(coofta.getColorNm());
+				temp.setColorNm(coofta.getColorNm());
 				temp.setTaste(coofta.getTaste());
-//				temp.setDate_of_registration(coofta.getDate_of_registration());
-//				temp.setUpdate_date(coofta.getUpdate_date());
-//				temp.setRegistration_userid(coofta.getRegistration_userid());
-//				temp.setUpdate_userid(coofta.getUpdate_userid());
-//				temp.setTime_stamp(coofta.getTime_stamp());
-//				temp.setDelete(coofta.getDelete());
+				// temp.setDate_of_registration(coofta.getDate_of_registration());
+				// temp.setUpdate_date(coofta.getUpdate_date());
+				// temp.setRegistration_userid(coofta.getRegistration_userid());
+				// temp.setUpdate_userid(coofta.getUpdate_userid());
+				// temp.setTime_stamp(coofta.getTime_stamp());
+				// temp.setDelete(coofta.getDelete());
 				tempTable.add(temp);
 			}
 		} catch (Exception e) {
