9b9a44c66169e7c8b24228c07dfa2c0223a330ff
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-27 05:52:48 +0000

    日付、USERidについて追加　江口

--- PC2015/src/model/CoofTa.java
+++ PC2015/src/model/CoofTa.java
@@ -15,12 +15,13 @@ public class CoofTa implements Serializable {
 	private int id;
 	private String colorNm;
 	private String taste;
-//	private String date_of_registration;
-//	private String update_date;
-//	private String registration_userid;
-//	private String update_userid;
-//	private int time_stamp;
-//	private int delete;
+	private String day;
+	private String new_day;
+	private String userid;
+	private String new_userid;
+
+	// private int time_stamp;
+	// private int delete;
 
 	@Id
 	@GeneratedValue
@@ -39,35 +40,35 @@ public class CoofTa implements Serializable {
 		return taste;
 	}
 
-//	@Column(name = "date_of_registration")
-//	public String getDate_of_registration() {
-//		return date_of_registration;
-//	}
-//
-//	@Column(name = "update_date")
-//	public String getUpdate_date() {
-//		return update_date;
-//	}
-//
-//	@Column(name = "registration_userid")
-//	public String getRegistration_userid() {
-//		return registration_userid;
-//	}
-//
-//	@Column(name = "update_userid")
-//	public String getUpdate_userid() {
-//		return update_userid;
-//	}
-//
-//	@Column(name = "time_stamp")
-//	public int getTime_stamp() {
-//		return time_stamp;
-//	}
+	@Column(name = "day")
+	public String getDay() {
+		return day;
+	}
 
-//	@Column(name = "delete")
-//	public int getDelete() {
-//		return delete;
-//	}
+	@Column(name = "new_day")
+	public String getNew_day() {
+		return new_day;
+	}
+
+	@Column(name = "userid")
+	public String getUserid() {
+		return userid;
+	}
+
+	@Column(name = "new_userid")
+	public String getNew_userid() {
+		return new_userid;
+	}
+
+	// @Column(name = "time_stamp")
+	// public int getTime_stamp() {
+	// return time_stamp;
+	// }
+
+	// @Column(name = "delete")
+	// public int getDelete() {
+	// return delete;
+	// }
 
 	public void setId(int id) {
 		this.id = id;
@@ -80,24 +81,26 @@ public class CoofTa implements Serializable {
 	public void setTaste(String taste) {
 		this.taste = taste;
 	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public void setNew_day(String new_day) {
+		this.new_day = new_day;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public void setNew_userid(String new_userid) {
+		this.new_userid = new_userid;
+	}
 }
-//	public void setDate_of_registration(String date_of_registration) {
-//		this.date_of_registration = date_of_registration;
-//	}
-//	
-//	public void setUpdate_date(String update_date) {
-//		this.update_date = update_date;
-//	}
+
 //
-//	public void setRegistration_userid(String registration_userid) {
-//		this.registration_userid = registration_userid;
-//	}
-//
-//	public void setUpdate_userid(String update_userid) {
-//		this.update_userid = update_userid;
-//	}
-//
-//	public void setTime_stamp(int time_stamp) {
-//		this.time_stamp = time_stamp;
-//	}
+// public void setTime_stamp(int time_stamp) {
+// this.time_stamp = time_stamp;
+// }
 
