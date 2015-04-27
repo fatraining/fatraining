2e1d1fa52d83dd997ee4dc60dfb5d2b91dad1411
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-27 02:15:15 +0000

    更新日時とユーザーIdをデータベースに登録できるようにしました。

--- PC2015/src/model/Movie.java
+++ PC2015/src/model/Movie.java
@@ -18,10 +18,10 @@ public class Movie implements Serializable{
 	private String title ;
 	private String genreId ;
 	private String exhibition_year ;
-//	private String registration_date;
-//	private String renewal_date;
-//	private String registration_userid;
-//	private String renewal_userid ;
+	private String registration_date;
+	private String renewal_date;
+	private String registration_userid;
+	private String renewal_userid ;
 //	private int control;
 //	private int delete ;
 	/*--------------------------------------------------*/
@@ -43,22 +43,22 @@ public class Movie implements Serializable{
 	public String getExhibition_year() {
 		return exhibition_year;
 	}
-//	@Column(name="registration_date")
-//	public String getRegistration_date() {
-//		return registration_date;
-//	}
-//	@Column(name="renewal_date")
-//	public String getRenewal_date() {
-//		return renewal_date;
-//	}
-//	@Column(name="registration_userid")
-//	public String getRegistration_userid() {
-//		return registration_userid;
-//	}
-//	@Column(name="renewal_userid")
-//	public String getRenewal_userid() {
-//		return renewal_userid;
-//	}
+	@Column(name="registration_date")
+	public String getRegistration_date() {
+		return registration_date;
+	}
+	@Column(name="renewal_date")
+	public String getRenewal_date() {
+		return renewal_date;
+	}
+	@Column(name="registration_userid")
+	public String getRegistration_userid() {
+		return registration_userid;
+	}
+	@Column(name="renewal_userid")
+	public String getRenewal_userid() {
+		return renewal_userid;
+	}
 //	@Column(name="control")
 //	public int getControl() {
 //		return control;
@@ -81,18 +81,18 @@ public class Movie implements Serializable{
 	public void setExhibition_year(String exhibition_year) {
 		this.exhibition_year = exhibition_year;
 	}
-//	public void setRegistration_date(String registration_date) {
-//		this.registration_date = registration_date;
-//	}
-//	public void setRenewal_date(String renewal_date) {
-//		this.renewal_date = renewal_date;
-//	}
-//	public void setRegistration_userid(String registration_userid) {
-//		this.registration_userid = registration_userid;
-//	}
-//	public void setRenewal_userid(String renewal_userid) {
-//		this.renewal_userid = renewal_userid;
-//	}	
+	public void setRegistration_date(String registration_date) {
+		this.registration_date = registration_date;
+	}
+	public void setRenewal_date(String renewal_date) {
+		this.renewal_date = renewal_date;
+	}
+	public void setRegistration_userid(String registration_userid) {
+		this.registration_userid = registration_userid;
+	}
+	public void setRenewal_userid(String renewal_userid) {
+		this.renewal_userid = renewal_userid;
+	}	
 //	public void setControl(int control) {
 //		this.control = control;
 //	}
