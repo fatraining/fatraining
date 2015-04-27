6fe2eef0b763844ac878fa8e5140bf73fb2d025e
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-27 02:07:46 +0000

    少し内容を修正・変更しました。篠崎

--- PC2015/src/menu6.sql
+++ PC2015/src/menu6.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2015 年 4 朁E23 日 14:50
+-- Generation Time: 2015 年 4 朁E27 日 09:15
 -- サーバのバージョン： 5.6.21
 -- PHP Version: 5.6.3
 
@@ -15,11 +15,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `task_master`
---
-
 -- --------------------------------------------------------
 
 --
@@ -36,17 +31,20 @@ CREATE TABLE IF NOT EXISTS `like_game` (
   `upUser` varchar(50) DEFAULT NULL,
   `nonStyle` varchar(50) DEFAULT NULL,
   `del` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- テーブルのデータのダンプ `like_game`
 --
 
 INSERT INTO `like_game` (`id`, `title`, `series`, `user`, `upDay`, `userId`, `upUser`, `nonStyle`, `del`) VALUES
-(1, 'Fate/stay night', 1, '4/21', '4/21', 'ShirouEmiya', '1', 'Blade Works', 'Sword'),
+(1, 'Fate/stay night', 1, '4/21', '4/21', 'ShirouEmiya', '1', 'Unlimited Blade Works', 'Sword'),
 (2, 'Perusona3', 2, '4/22', '4/22', 'ShionAikawa', '2', 'Orpheus', 'Wild'),
 (3, 'FAINALFANTASY7', 3, '4/22', '4/22', 'Cloud', '3', 'MeteorStrike', 'Soldier'),
-(4, 'Fate/hollow', 1, NULL, '4/23', 'RinTosaka', '4', 'JewelrySword', 'Money');
+(4, 'Fate/hollow', 1, NULL, '4/23', 'RinTosaka', '4', 'JewelrySword', 'Money'),
+(5, 'LORD of VERMILION', 5, NULL, '4/24', 'Alma=Calma', '5', 'Bahamut', 'Arcana'),
+(6, 'Fate/EXTRA', 1, NULL, '4/24', 'HakunoKishinami', '6', 'Amaterasu', 'Nobody'),
+(7, 'Fate/EXTRA CCC', 1, NULL, '4/24', 'BB', '7', 'C.C.C', 'MoonQeen');
 
 -- --------------------------------------------------------
 
@@ -63,48 +61,23 @@ CREATE TABLE IF NOT EXISTS `like_series` (
   `upU` varchar(50) DEFAULT NULL,
   `nonS` varchar(50) DEFAULT NULL,
   `d` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- テーブルのデータのダンプ `like_series`
 --
 
 INSERT INTO `like_series` (`i`, `se`, `u`, `upD`, `userI`, `upU`, `nonS`, `d`) VALUES
-(1, 'Fate', '4/21', '4/21', 'ShirouEmiya', '1', 'Blade Works', 'Sword'),
+(1, 'Fate', '4/21', '4/21', 'ShirouEmiya', '1', 'Unlimited Blade Works', 'Sword'),
 (2, 'Perusona', '4/22', '4/22', 'ShionAikawa', '2', 'Orpheus', 'Wild'),
 (3, 'FAINALFANTASY', '4/22', '4/22', 'Cloud', '3', 'MeteorStrike', 'Soldier'),
-(4, NULL, '4/23', NULL, NULL, NULL, NULL, NULL);
+(4, NULL, '4/23', NULL, NULL, NULL, NULL, NULL),
+(5, 'LORD of VERMILION', '4/24', NULL, NULL, NULL, NULL, NULL),
+(6, 'Fate', '4/24', NULL, NULL, NULL, NULL, NULL),
+(7, 'Fate', '4/24', NULL, NULL, NULL, NULL, NULL);
 
---
--- Indexes for dumped tables
---
+-- --------------------------------------------------------
 
---
--- Indexes for table `like_game`
---
-ALTER TABLE `like_game`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `like_series`
---
-ALTER TABLE `like_series`
- ADD PRIMARY KEY (`i`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `like_game`
---
-ALTER TABLE `like_game`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'AUTO_INCERMENT',AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `like_series`
---
-ALTER TABLE `like_series`
-MODIFY `i` int(11) NOT NULL AUTO_INCREMENT COMMENT 'AUTO_INCREMENT',AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
