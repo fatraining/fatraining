687efde56e9ac6c4437319c9c05c56f7a9784b07
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-27 05:51:44 +0000

    コメントの追加 江口

--- PC2015/src/action/Main9Action.java
+++ PC2015/src/action/Main9Action.java
@@ -10,36 +10,40 @@ import model.Result9Table;
 public class Main9Action extends AbstractAction {
 
 	private static final long serialVersionUID = 1L;
-
+	// カラム
 	public String name;
 	public String food;
 	public String drink;
 	public String search;
+	// 変数
 	public String result;
 	public String update_id;
 	public String delete;
 	public String do_print;
 
-	private Result9Manager linkController;
+	public Result9Manager linkController;
 	private LiofTaManager allController;
 	public ArrayList<Result9Table> outputTable;
 
-
+	// 変数に値を代入する（この場合空白）
 	private String getDefaultName() {
 		this.name = "";
 		return this.name;
 	}
 
+	// 変数に値を代入する（この場合空白）
 	private String getDefaultFood() {
 		this.food = "";
 		return this.food;
 	}
 
+	// 変数に値を代入する（この場合空白）
 	private String getDefaultDrink() {
 		this.drink = "";
 		return this.drink;
 	}
 
+	// executeメソッド
 	@Override
 	public String execute() {
 		this.name = getDefaultName();
@@ -49,6 +53,7 @@ public class Main9Action extends AbstractAction {
 		return "success";
 	}
 
+	// resetメソッド
 	public String reset() {
 		this.name = getDefaultName();
 		this.food = getDefaultFood();
@@ -56,6 +61,7 @@ public class Main9Action extends AbstractAction {
 		return "success";
 	}
 
+	// searchメソッド
 	public String search() {
 		this.do_print = "true";
 		if (this.name.isEmpty() && this.food.isEmpty() && this.drink.isEmpty()) {
@@ -73,14 +79,16 @@ public class Main9Action extends AbstractAction {
 		return "success";
 	}
 
+	// printallメソッド
 	public String printall() {
 		this.do_print = "true";
 		allController = new LiofTaManager();
-		this.outputTable = allController.resultList();
-		this.delete = "true";
+		this.outputTable = allController.resultList(); // テーブル取得
+		this.delete = "true"; // trueの場合削除ボタンを表示する
 		return "success";
 	}
 
+	// updateメソッド
 	public String update() {
 		this.sessionMap.put("update_id", this.update_id);
 		try {
