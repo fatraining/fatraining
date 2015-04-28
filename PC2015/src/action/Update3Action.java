7f2580cade725f407082726b2ef17d8b0f7a6df3
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-28 03:31:12 +0000

    河野

--- PC2015/src/action/Update3Action.java
+++ PC2015/src/action/Update3Action.java
@@ -17,25 +17,23 @@ public class Update3Action extends AbstractAction {
 	public String title;
 	public String genre;
 	public int genre_id;
-//	public String signup;
-//	public String update;
-//	public String signupUSERID;
-//	public String updateUSERID;
-//	public String exclusiveFLG;
-//  public String deleteFLG;
-    public String do_print;
+	// public String signup;
+	// public String update;
+	// public String signupUSERID;
+	// public String updateUSERID;
+	// public String exclusiveFLG;
+	// public String deleteFLG;
+	public String do_print;
 	public String update_id;
 	public String errormsg;
 
 	public String execute() throws Exception {
-		this.update_id = (String)this.sessionMap.get("update_id");
-
-
+		this.update_id = (String) this.sessionMap.get("update_id");
 
 		return "success";
 	}
 
-	public String insert(){
+	public String insert() {
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
 
@@ -44,52 +42,57 @@ public class Update3Action extends AbstractAction {
 		insert_genre_table.setGenre(this.genre);
 		insert_story_table.setTitle(this.title);
 		insert_story_table.setGenre_id(this.genre_id);
-//		insert_story_table.setSignup(this.signup);
-//		insert_story_table.setUpdate(this.update);
-//		insert_story_table.setSignupUSERID(this.signupUSERID);
-//		insert_story_table.setUpdateUSERID(this.updateUSERID);
-//		insert_story_table.setExclusiveFLG(this.exclusiveFLG);
-//		insert_story_table.setdeleteFLG(this.deleteFLG);
-//		
-//		String[] data = {this.update_id,this.title,this.genre,this.signup,this.update,this.signupUSERID,this.updateUSERID,this.exclusiveFLG,this.deleteFLG};
-//		int i=0;
-//		for(String temp : data){
-//			if(temp.length()>50){
-//				this.errormsg = "50�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽﾈ会ｿｽ�ｿｽﾅ難ｿｽ�ｿｽﾍゑｿｽ�ｿｽﾄゑｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ";
-//				return "error";
-//			}
-//			if(temp.length()<1)i++;
-//			if(i>14){
-//				this.errormsg = "�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽﾍは登�ｿｽ^�ｿｽﾅゑｿｽ�ｿｽﾜゑｿｽ�ｿｽ�ｿｽ";
-//				return "error";
-//			}
+		// insert_story_table.setSignup(this.signup);
+		// insert_story_table.setUpdate(this.update);
+		// insert_story_table.setSignupUSERID(this.signupUSERID);
+		// insert_story_table.setUpdateUSERID(this.updateUSERID);
+		// insert_story_table.setExclusiveFLG(this.exclusiveFLG);
+		// insert_story_table.setdeleteFLG(this.deleteFLG);
+		//
+		// String[] data =
+		// {this.update_id,this.title,this.genre,this.signup,this.update,this.signupUSERID,this.updateUSERID,this.exclusiveFLG,this.deleteFLG};
+		// int i=0;
+		// for(String temp : data){
+		// if(temp.length()>50){
+		// this.errormsg =
+		// "50�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽﾈ会ｿｽ�ｿｽﾅ難ｿｽ�ｿｽﾍゑｿｽ�ｿｽﾄゑｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ";
+		// return "error";
+		// }
+		// if(temp.length()<1)i++;
+		// if(i>14){
+		// this.errormsg = "�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽﾍは登�ｿｽ^�ｿｽﾅゑｿｽ�ｿｽﾜゑｿｽ�ｿｽ�ｿｽ";
+		// return "error";
+		// }
 
-			try{
-				session.save(insert_story_table);
-				session.save(insert_genre_table);
+		try {
+			session.save(insert_story_table);
+			session.save(insert_genre_table);
 
-			}catch(HibernateException e){
-				e.printStackTrace();
-				session.getTransaction().rollback();
-			}
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		}
 
-//		}
+		// }
 		session.getTransaction().commit();
 		return "main3";
 
 	}
+
 	public String delete() {
-		this.update_id = (String)this.sessionMap.get("update_id");
-		if(this.update_id.isEmpty()){
+		this.update_id = (String) this.sessionMap.get("update_id");
+		if (this.update_id.isEmpty()) {
 			return "main3";
 		}
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
 		try {
-			Story detailpc = (Story)session.load(Story.class, update_id);
-			Tb_Genre idpc = (Tb_Genre) session.load(Tb_Genre.class, update_id);
-			session.delete(detailpc);
-			session.delete(idpc);
+			Story story = (Story) session.load(Story.class,
+					Integer.valueOf(update_id));
+			Tb_Genre tb_genre = (Tb_Genre) session.load(Tb_Genre.class,
+					Integer.valueOf(update_id));
+			session.delete(story);
+			session.delete(tb_genre);
 		} catch (HibernateException e) {
 			e.printStackTrace();
 			session.getTransaction().rollback();
@@ -97,17 +100,19 @@ public class Update3Action extends AbstractAction {
 		session.getTransaction().commit();
 		return "main3";
 	}
-//	
-//	
-//	private static boolean checkCharacterCode(String str, String encoding) {
-//		if (str == null) {
-//			return true;
-//		}
-//
-//		try {
-//			byte[] bytes = str.getBytes(encoding);
-//			return str.equals(new String(bytes, encoding));
-//		} catch (UnsupportedEncodingException ex) {
-//			throw new RuntimeException("�ｿｽG�ｿｽ�ｿｽ�ｿｽR�ｿｽ[�ｿｽh�ｿｽ�ｿｽ�ｿｽﾌゑｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽﾜゑｿｽ�ｿｽ�ｿｽB", ex);
-//		}
-	}
+	//
+	//
+	// private static boolean checkCharacterCode(String str, String encoding) {
+	// if (str == null) {
+	// return true;
+	// }
+	//
+	// try {
+	// byte[] bytes = str.getBytes(encoding);
+	// return str.equals(new String(bytes, encoding));
+	// } catch (UnsupportedEncodingException ex) {
+	// throw new
+	// RuntimeException("�ｿｽG�ｿｽ�ｿｽ�ｿｽR�ｿｽ[�ｿｽh�ｿｽ�ｿｽ�ｿｽﾌゑｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽ�ｿｽﾜゑｿｽ�ｿｽ�ｿｽB",
+	// ex);
+	// }
+}
