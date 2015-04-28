f0d8440edebeb23c1f9a0360c221beb2c96842d6
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-28 07:13:15 +0000

    コメントなど変更しました。帆刈

--- PC2015/src/action/Update2Action.java
+++ PC2015/src/action/Update2Action.java
@@ -10,26 +10,33 @@ import controller.HibernateUtil;
 import controller.SweetsManager;
 import controller.Result2Manager;
 
-import java.util.*;
-import java.text.*;
 
+import org.apache.struts2.dispatcher.ServletRedirectResult;
 import org.hibernate.HibernateException;
 import org.hibernate.classic.Session;
 
-public class Update2Action extends AbstractAction {
+//import java.util.*;
+//import java.text.*;
+//import java.io.UnsupportedEncodingException;
+//import java.text.SimpleDateFormat;
+//
+//import model.BandAccount;
+//import model.BandTable;
 
+import org.apache.struts2.config.Result;
+//import controller.HibernateUtil;
+
+@Result(name = "main2", value = "main2.action", type = ServletRedirectResult.class)
+public class Update2Action extends AbstractAction {
 	private static final long serialVersionUID = 1L;
 
 	public String id;
 	public String name;
 	public String genreNm;
-	public String entry_userId;
-	public String record_userId;
-	public String exclusive_FLG;
-	public String delete_FLG;
 	public String update_id;
 	public String do_search;
 	public String errormsg;
+	public String delete;
 
 	private Result2Manager linkController;
 	private SweetsManager allController;
@@ -45,32 +52,18 @@ public class Update2Action extends AbstractAction {
 		return this.genreNm;
 	}
 
-	private String getDefaultEntry_userId() {
-		this.entry_userId = "";
-		return this.entry_userId;
-	}
-
-	private String getDefaultRecord_userId() {
-		this.record_userId = "";
-		return this.record_userId;
-	}
-
-	private String getDefaultExclusive_FLG() {
-		this.exclusive_FLG = "";
-		return this.exclusive_FLG;
-	}
-
-	private String getDefaultDelete_FLG() {
-		this.delete_FLG = "";
-		return this.delete_FLG;
-	}
-
+	
+	//insertメソッド
 	public String insert() {
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
 
-//		Sweets insert_genre_table = new Sweets();
-//		Genre insert_sweets_table = new Genre();
+		Sweets insert_sweets = new Sweets();
+		Genre insert_genre = new Genre();
+		insert_sweets.setName(this.name);
+		insert_genre.setGenreNm(this.genreNm);
+
+		
 //		String[] data = { this.genreNm, this.name };
 //		int i = 0;
 //		for (String temp : data) {
@@ -84,51 +77,78 @@ public class Update2Action extends AbstractAction {
 //				this.errormsg = "未入力は登録できません";
 //				return "error";
 //			}
-//			try {
-//				session.save(insert_sweets_table);
-//				session.save(insert_genre_table);
-//			} catch (HibernateException e) {
-//				e.printStackTrace();
-//				session.getTransaction().rollback();
-//			}
+			try {
+				session.save(insert_sweets);
+				session.save(insert_genre);
+			} catch (HibernateException e) {
+				e.printStackTrace();
+				session.getTransaction().rollback();
+			}
+			
 //		}
-//		session.getTransaction().commit();
-//		return "Main2";
-//	}
+		session.getTransaction().commit();
+		return "main2";
+	}
+	
+	//deleteメソッド
+	public String delete(){
+		this.update_id = (String)this.sessionMap.get("update_id");
+		
+		String str = new String(this.update_id);
+		String[] strAry = str.split(",");
+		
+		if (this.update_id.isEmpty()) {
+			return "main2";
+		}
+		
+		for(int i = 0; i < strAry.length; i++) {
+		
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		try {
+			Sweets sweets = (Sweets) session.load(Sweets.class, strAry[i]);
+			Genre genre = (Genre) session.load(Genre.class, strAry[i]);
+			session.delete(sweets);
+			session.delete(genre);
+		}catch (HibernateException e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		}
+		session.getTransaction().commit();
+		}
+		return "main2";
+	}
 //		public String getDefaultDate(){
 //		Date date = new Date();
 //		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd k:m:s");
 //		record_date = String.valueOf(sdf.format(date));
 //		reset_date = String.valueOf(sdf.format(date));
 //
-		return "date";
-	}
+//		return "date";
+//	}
 
+	//executeメソッド
 	@Override
 	public String execute() {
 		this.id = (String) this.sessionMap.get("id");
 		this.name = getDefaultName();
 		this.genreNm = getDefaultGenreNm();
-		this.entry_userId = getDefaultEntry_userId();
-		this.record_userId = getDefaultRecord_userId();
-		this.exclusive_FLG = getDefaultExclusive_FLG();
-		this.delete_FLG = getDefaultDelete_FLG();
+
 
 		return "success";
 	}
 
+	//resetメソッド
 	public String reset() {
 		this.name = getDefaultName();
 		this.genreNm = getDefaultGenreNm();
-		this.entry_userId = getDefaultEntry_userId();
-		this.record_userId = getDefaultRecord_userId();
-		this.exclusive_FLG = getDefaultExclusive_FLG();
-		this.delete_FLG = getDefaultDelete_FLG();
+
 
 		return "success";
 	}
 
-	public String print() {
+	//searchメソッド
+	public String search() {
 
 		this.do_search = "true";
 		this.id = (String) this.sessionMap.get("id");
@@ -143,24 +163,26 @@ public class Update2Action extends AbstractAction {
 			this.outputTable = linkController.resultList(this.genreNm,
 					this.name);
 		}
-		this.delete_FLG = "true";
+		this.delete = "true";
 		return "success";
 	}
 
+	//printallメソッド
 	public String printall() {
 		this.do_search = "true";
 		this.id = (String) this.sessionMap.get("id");
 		allController = new SweetsManager();
 		this.outputTable = allController.resultList(this.genreNm, this.name);
-		this.delete_FLG = "true";
+		this.delete = "true";
 		return "success";
 	}
 
+	//updateメソッド
 	public String update() {
 		 this.sessionMap.put("update_id", this.update_id);
 
 		try {
-			this.response.sendRedirect("/PC2015/updateAction");
+			this.response.sendRedirect("/PC2015/update2.action");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
