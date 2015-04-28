bf0dc24c0b2c822cf0a7e76633d4c9abf4ba79a4
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-28 08:44:03 +0000

    this=を一つにまとめました。横山

--- PC2015/src/action/Main10Action.java
+++ PC2015/src/action/Main10Action.java
@@ -9,7 +9,6 @@ import controller.Result10Manager;
 
 public class Main10Action extends AbstractAction {
 
-
 	private static final long serialVersionUID = 1L;
 
 	public String eat_year;
@@ -22,81 +21,72 @@ public class Main10Action extends AbstractAction {
 	public String delete;
 	public String userId;
 
-	private Result10Manager linkController;
-	private All10Manager allController;
-	public  ArrayList<Result10Table> outputTable;
+	public ArrayList<Result10Table> outputTable;
 
-	private String getDefaultEat_year(){//eat_yearのデフォルト値は2015
-		this.eat_year = "2015";
-		return this.eat_year;
-	}
-	private String getDefaultEat_month(){//eat_monthのデフォルト値は1
-		this.eat_month = "1";
-		return this.eat_month;
-	}
-	private String getDefaultEat_day(){//eat_dayのデフォルト値は1
-		this.eat_month = "1";
-		return this.eat_month;
-	}
-	private String getDefaultEat_hour(){//eat_hourのデフォルト値はAM
-		this.eat_hour = "AM";
-		return this.eat_hour;
+	private String getDefaultEat_year() {// eat_yearのデフォルト値は2015
+		return "2015";
 	}
 
-	@Override//スーパークラスのメソッドをサブクラスで書きなおすこと
-	public String execute() { //executeメソッドを作る（始めにデフォルト値を検索欄に表示させておくため）
-		this.userId = (String)this.sessionMap.get("userId"); // sessionMapに保存していたuserIDを取得
-		this.eat_year = getDefaultEat_year(); //デフォルト値の取得
-		this.eat_month   = getDefaultEat_month(); //デフォルト値の取得
-		this.eat_day = getDefaultEat_day(); //デフォルト値の取得
-		this.eat_hour = getDefaultEat_hour(); //デフォルト値の取得
-		this.delete="faluse"; //todo
+	private String getDefaultEat_month() {// eat_monthのデフォルト値は1
+		return "1";
+	}
+
+	private String getDefaultEat_day() {// eat_dayのデフォルト値は1
+		return "1";
+	}
+
+	private String getDefaultEat_hour() {// eat_hourのデフォルト値はAM
+		return "AM";
+	}
+
+	@Override
+	// スーパークラスのメソッドをサブクラスで書きなおすこと
+	public String execute() { // executeメソッドを作る（始めにデフォルト値を検索欄に表示させておくため）
+		this.userId = (String) this.sessionMap.get("userId"); // sessionMapに保存していたuserIDを取得
+		this.eat_year = getDefaultEat_year(); // デフォルト値の取得
+		this.eat_month = getDefaultEat_month(); // デフォルト値の取得
+		this.eat_day = getDefaultEat_day(); // デフォルト値の取得
+		this.eat_hour = getDefaultEat_hour(); // デフォルト値の取得
+		this.delete = "false"; // todo
 		return "success";
 	}
-	
-	public String reset(){ //resetメソッドを作る
-		this.userId = (String)this.sessionMap.get("userId"); //userIdの取得
-		this.eat_year = getDefaultEat_year(); //デフォルト値の取得
-		this.eat_month   = getDefaultEat_month(); //デフォルト値の取得
-		this.eat_day = getDefaultEat_day(); //デフォルト値の取得
-		this.eat_hour = getDefaultEat_hour(); //デフォルト値の取得
-		return "success";
-	}
-	
-	public String search(){ //searchメソッド
-		this.do_search = "true"; //ボタンで処理する
-		this.userId = (String)this.sessionMap.get("userId");
-		if(this.eat_year.isEmpty() && this.eat_month.isEmpty() && this.eat_day.isEmpty() && this.eat_hour.isEmpty()){
-			//もしすべて空だった場合は
+
+	public String search() { // searchメソッド
+		this.do_search = "true"; // ボタンで処理する
+		this.userId = (String) this.sessionMap.get("userId");
+		if (this.eat_year.isEmpty() && this.eat_month.isEmpty()
+				&& this.eat_day.isEmpty() && this.eat_hour.isEmpty()) {
+			// もしすべて空だった場合は
 			try {
-				searchall(); //searchallメソッドを呼び出す
-			} catch (Exception e) { //例外キャッチ
-				e.printStackTrace(); //todo
+				searchall(); // searchallメソッドを呼び出す
+			} catch (Exception e) { // 例外キャッチ
+				e.printStackTrace(); // todo
 			}
-		}else{ //空でなかったら 検索結果の表示をする
-			linkController  = new Result10Manager(); //インスタンスの生成
-			this.outputTable = linkController.resultList(this.eat_year,this.eat_month,this.eat_day,this.eat_hour);
+		} else { // 空でなかったら 検索結果の表示をする
+			Result10Manager linkController = new Result10Manager(); // インスタンスの生成
+			this.outputTable = linkController.resultList(this.eat_year,
+					this.eat_month, this.eat_day, this.eat_hour);
 		}
-		this.delete="true"; //deleteボタンができる
-		return "success"; //searchが成功する
+		this.delete = "true"; // deleteボタンができる
+		return "success"; // searchが成功する
 	}
-	
-	public String searchall(){ //searchallメソッド
-		this.do_search = "true"; //do_searchのボタン機能（searchとは同じボタンでsearchallする）
-		allController  = new All10Manager(); //todo
-		this.outputTable = allController.resultList(); //searchよりsearchallは値が空のときについてなので引数なし
-		this.delete="true"; //deleteボタンができる
-		return "success"; //空欄での全検索ができるようになる
+
+	public String searchall() { // searchallメソッド
+		this.do_search = "true"; // do_searchのボタン機能（searchとは同じボタンでsearchallする）
+		All10Manager allController = new All10Manager(); // todo
+		this.outputTable = allController.resultList(); // searchよりsearchallは値が空のときについてなので引数なし
+		this.delete = "true"; // deleteボタンができる
+		return "success"; // 空欄での全検索ができるようになる
 	}
-	
-	public String update(){ //updateメソッド
-		this.sessionMap.put("update_id",this.update_id);//update_idを取得する
-		try {//例外が発生するかもしれない処理
-			this.response.sendRedirect("/PC2015/update10.action"); //UpdateActionに接続する
-		} catch (IOException e) { //例外をキャッチ
+
+	public String update() { // updateメソッド
+		this.sessionMap.put("update_id", this.update_id);// update_idを取得する
+		try {// 例外が発生するかもしれない処理
+			this.response.sendRedirect("/PC2015/update10.action"); // UpdateActionに接続する
+		} catch (IOException e) { // 例外をキャッチ
 			e.printStackTrace();
 		}
 
-		return "success"; //updateができる
+		return "success"; // updateができる
 	}
 }
