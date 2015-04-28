6a7c0f2df914908706a3061776226830c0b7b8f9
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-28 03:31:44 +0000

    河野

--- PC2015/src/controller/Result3Manager.java
+++ PC2015/src/controller/Result3Manager.java
@@ -11,26 +11,27 @@ import org.hibernate.classic.Session;
 
 public class Result3Manager extends HibernateUtil {
 
-	public  List<?> resultTable;
-	public  ArrayList<Result3Table> outputTable;
+	public ArrayList<Result3Table> outputTable;
+	public List<?> resultTable;
 
-	public ArrayList<Result3Table> resultList(String title,String genre){
+	public ArrayList<Result3Table> resultList(String title, String genre) {
 
-		Session session = HibernateUtil.getSessionFactory()
-				.getCurrentSession();
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
 		try {
-			if(title.isEmpty())title="%";
-			if(genre.isEmpty())genre="%";
-			
+			if (title.isEmpty())
+				title = "%";
+			if (genre.isEmpty())
+				genre = "%";
+
 			String select = "SELECT * FROM story d,tb_genre i";
 			String where1 = "WHERE d.genre_id = i.id";
-			String where2 = "AND (i.title LIKE '"+ title +"' AND i.genre LIKE '"
-					+ genre + ")";
+			String where2 = "AND (i.title LIKE '" + title
+					+ "' AND i.genre LIKE '" + genre + ")";
 			String sql = select + " " + where1 + " " + where2;
 			resultTable = session.createSQLQuery(sql)
 					.addEntity("Story", Story.class)
-					.addEntity("Genre", Story.class).list();
+					.addEntity("Tb_Genre", Story.class).list();
 		} catch (Exception e) {
 			e.printStackTrace();
 			session.getTransaction().rollback();
@@ -39,24 +40,25 @@ public class Result3Manager extends HibernateUtil {
 		this.outputTable = tableTrans(resultTable);
 		return outputTable;
 	}
-	public ArrayList<Result3Table> tableTrans(List<?> resultTable){
+
+	public ArrayList<Result3Table> tableTrans(List<?> resultTable) {
 		ArrayList<Result3Table> tempTable = new ArrayList<Result3Table>();
 		Object[] obj;
 		try {
-			for(int i = 0 ; i < resultTable.size() ; i++){
+			for (int i = 0; i < resultTable.size(); i++) {
 				Result3Table temp = new Result3Table();
 				obj = (Object[]) resultTable.get(i);
-				Story story =  (Story)obj[0];
-				Tb_Genre genre     = (Tb_Genre)obj[1];
-				temp.setGenre(genre .getGenre());
+				Story story = (Story) obj[0];
+				Tb_Genre tb_genre = (Tb_Genre) obj[1];
+				temp.setGenre(tb_genre.getGenre());
 				temp.setId(story.getId());
 				temp.setTitle(story.getTitle());
-//				temp.setSignup(story.getSignup());
-//				temp.setUPdate(story.getUpdate());
-//				temp.setSignupUSERID(story.getSignupUSERID());
-//				temp.setUpdateUSERID(story.getUpdateUSERID());
-//				temp.setExclusiveFLG(story.getExclusiveFLG());
-//				temp.setDeleteFLG(story.getDeleteFLG());
+				// temp.setSignup(story.getSignup());
+				// temp.setUPdate(story.getUpdate());
+				// temp.setSignupUSERID(story.getSignupUSERID());
+				// temp.setUpdateUSERID(story.getUpdateUSERID());
+				// temp.setExclusiveFLG(story.getExclusiveFLG());
+				// temp.setDeleteFLG(story.getDeleteFLG());
 				tempTable.add(temp);
 			}
 		} catch (Exception e) {
