53781b50f6c3dd81516279fc2e682a198364fab1
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-28 07:57:46 +0000

    コメントの追加

--- PC2015/src/action/Main5Action.java
+++ PC2015/src/action/Main5Action.java
@@ -2,6 +2,7 @@ package action;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import model.Result5Table;
 import controller.Result5Manager;
@@ -11,71 +12,88 @@ public class Main5Action extends AbstractAction {
 
 	private static final long serialVersionUID = 1L;
 
-//	public String user_name;
+	// 表示させるカラム名
+	// public String user_name;
 	public String dwelling;
 	public String name;
-	public String result;
+
+	//検索結果の変数
 	public String do_search;
 	public String update_id;
+	//削除ボタンの変数
 	public String delete;
 
+	//検索結果の表示
 	private Result5Manager linkController;
 	private User_ProfileManager allController;
 	public ArrayList<Result5Table> outputTable;
+	
+	public List<?> resultTable;
 
+
+	// 郵便番号の変数に値を代入(空白)
 	private String getDefaultDwelling() {
-		this.dwelling = "千葉";
+		this.dwelling = "";
 		return this.dwelling;
 	}
 
+	// 名前の変数に値を代入(空白)
 	private String getDefaultName() {
-		this.name = "田中";
+		this.name = "";
 		return this.name;
 	}
 
 	@Override
 	public String execute() {
-//		this.user_name = (String) this.sessionMap.get("user_name");
+		// user_nameを表示させる為にsessionMapから呼び出している
+		// this.user_name = (String) this.sessionMap.get("user_name");
 
 		this.dwelling = getDefaultDwelling();
 		this.name = getDefaultName();
+
+		// 削除ボタン
 		this.delete = "faluse";
 		return "success";
 	}
 
+	// リセットメソッド 値を表示させないようにする為、getDefaultDwellingとgetDefaultNameを代入
 	public String reset() {
-//		this.user_name = (String) this.sessionMap.get("user_name");
+		// this.user_name = (String) this.sessionMap.get("user_name");
 		this.dwelling = getDefaultDwelling();
 		this.name = getDefaultName();
 		return "success";
 	}
 
+	// 検索メソッド
 	public String search() {
 
-		// this.dwelling = checkcode(this.dwelling);
-		// this.name = checkcode(this.name);
-
 		this.do_search = "true";
-//		this.user_name = (String) this.sessionMap.get("user_name");
+		// this.user_name = (String) this.sessionMap.get("user_name");
+		//入力した値が空だった場合、値を全て表示させる
 		if (this.dwelling.isEmpty() && this.name.isEmpty()) {
+			//
 			try {
 				printall();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		} else {
-			linkController = new Result5Manager();
+			//入力した値が空ではない場合、Result5Manager内のdwellingとnameを表示させる
+			Result5Manager linkController = new Result5Manager();
 			this.outputTable = linkController.resultList(this.dwelling,
 					this.name);
 		}
+		//削除ボタンの表示
 		this.delete = "true";
 		return "success";
 	}
 
 	public String printall() {
+		//検索結果の表示
 		this.do_search = "true";
 		allController = new User_ProfileManager();
 		this.outputTable = allController.resultList();
+		//削除ボタンの表示
 		this.delete = "true";
 		return "success";
 	}
@@ -84,6 +102,7 @@ public class Main5Action extends AbstractAction {
 		this.sessionMap.put("update_id", this.update_id);
 
 		try {
+			//update5Actionに進む
 			this.response.sendRedirect("/PC2015/update5.action");
 		} catch (IOException e) {
 			e.printStackTrace();
--- PC2015/src/action/Update5Action.java
+++ PC2015/src/action/Update5Action.java
@@ -1,6 +1,7 @@
 package action;
 
-import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 import model.User_Profile;
 import model.User_Character;
@@ -15,6 +16,8 @@ import controller.HibernateUtil;
 @Result(name = "main5", value = "main5.action", type = ServletRedirectResult.class)
 public class Update5Action extends AbstractAction {
 	private static final long serialVersionUID = 1L;
+
+	// テーブル内のカラム名の宣言
 	public String update_id;
 	public String errormsg;
 	public int id;
@@ -32,18 +35,30 @@ public class Update5Action extends AbstractAction {
 	public String interest;
 
 	public String execute() throws Exception {
+		//Main5Actionのupdate_idを呼び出している
 		this.update_id = (String) this.sessionMap.get("update_id");
 
 		return "success";
 	}
 
 	public String insert() {
+		// 登録、更新日時設定
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd k:m:s");
+		day = String.valueOf(sdf.format(date));
+		newday = String.valueOf(sdf.format(date));
+
+		// 登録、更新UAER設定
+		this.userid = (String) this.sessionMap.get("userId");
+		this.newuserid = (String) this.sessionMap.get("userId");
 
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
 
+		//User_ProfileクラスとUser_Characterクラスのインスタンス化
 		User_Profile insert_user_profile_table = new User_Profile();
 		User_Character insert_user_character_table = new User_Character();
+		
 		insert_user_profile_table.setId(this.id);
 		insert_user_profile_table.setPhonenumber(this.phonenumber);
 		insert_user_profile_table.setZipcode(this.zipcode);
@@ -54,24 +69,13 @@ public class Update5Action extends AbstractAction {
 		insert_user_profile_table.setNewday(this.newday);
 		insert_user_profile_table.setUserid(this.userid);
 		insert_user_profile_table.setNewuserid(this.newuserid);
-		// insert_user_profile_table.setDelete((this.delete));
-		// insert_user_profile_table.setFlg((this.flg));
 		insert_user_character_table.setInterest(this.interest);
 		String[] data = { this.dwelling, this.name, this.day, this.newday,
 				this.userid, this.newuserid, this.interest, this.personality };
 		int i = 0;
 		for (String temp : data) {
-//			if (temp.length() > 50) {
-//				this.errormsg = "50文字以下で入力してください";
-//				return "error";
-//			}
-//			if (temp.length() < 1)
-//				i++;
-//			if (i > 14) {
-//				this.errormsg = "未入力は登録できません";
-//				return "error";
-//			}
 
+			// user_profileテーブルとuser_characterテーブルに値を追加
 			try {
 				session.save(insert_user_profile_table);
 				session.save(insert_user_character_table);
@@ -83,22 +87,26 @@ public class Update5Action extends AbstractAction {
 
 		}
 		session.getTransaction().commit();
+		//main5に戻る
 		return "main5";
 
 	}
 
+	// 検索結果内の値を削除
 	public String delete() {
 		this.update_id = (String) this.sessionMap.get("update_id");
+		//もしupdate_idが空の場合はmain5に戻る
 		if (this.update_id.isEmpty()) {
 			return "main5";
 		}
+		// サーバー内のテーブルの値を削除
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
 		try {
 			User_Profile profile = (User_Profile) session.load(
-					User_Profile.class, update_id);
+					User_Profile.class, Integer.valueOf(update_id));
 			User_Character character = (User_Character) session.load(
-					User_Character.class, update_id);
+					User_Character.class, Integer.valueOf(update_id));
 			session.delete(profile);
 			session.delete(character);
 		} catch (HibernateException e) {
@@ -106,29 +114,7 @@ public class Update5Action extends AbstractAction {
 			session.getTransaction().rollback();
 		}
 		session.getTransaction().commit();
+		//main5に戻る
 		return "main5";
 	}
 }
-//
-//	public String checkcode(String code) {
-//
-//		// code = code.replaceAll("[^a-zA-Z_0-9]","_");
-//		if (!code.matches("[a-zA-Z_0-9]{0,50}")) {
-//			// return "";
-//		}
-//		return code;
-//	}
-//
-//	private static boolean checkCharacterCode(String str, String encoding) {
-//		if (str == null) {
-//			return true;
-//		}
-//
-//		try {
-//			byte[] bytes = str.getBytes(encoding);
-//			return str.equals(new String(bytes, encoding));
-//		} catch (UnsupportedEncodingException ex) {
-//			throw new RuntimeException("エンコード名称が正しくありません。", ex);
-//		}
-//	}
-//}
