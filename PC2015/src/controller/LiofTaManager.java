8410b18bcef6ae0dd327628b49fc6e85458c2e09
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-30 06:04:16 +0000

    表示を出来るように修正しました。　江口

--- PC2015/src/controller/LiofTaManager.java
+++ PC2015/src/controller/LiofTaManager.java
@@ -1,26 +1,23 @@
 package controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import model.LiofTa;
 import model.CoofTa;
-import model.Result9Table;
 
 import org.hibernate.classic.Session;
 
 public class LiofTaManager extends HibernateUtil {
-	public List<?> result9Table;
-	public ArrayList<Result9Table> outputTable;
+	public List<?> resultTable;
 
-	public ArrayList<Result9Table> resultList() {
+	public List<?> resultList() {
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
 		String select = "SELECT * FROM table_like d,table_color i";
 		String where1 = "WHERE d.id=i.id";
 		String sql = select + " " + where1;
 		try {
-			result9Table = session.createSQLQuery(sql)
+			resultTable = session.createSQLQuery(sql)
 					.addEntity("table_like", LiofTa.class)
 					.addEntity("table_color", CoofTa.class).list();
 		} catch (Exception e) {
@@ -29,12 +26,10 @@ public class LiofTaManager extends HibernateUtil {
 		}
 		session.getTransaction().commit(); // Transaction処理が成功したとき結果を確立させる
 
-		this.outputTable = tableTrans(result9Table); // テーブル取得
-
-		return outputTable;
+		return resultTable;
 	}
 
-	public ArrayList<Result9Table> resultList(String name, String food,
+	public List<?> resultList(String name, String food,
 			String drink) {
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
@@ -52,7 +47,7 @@ public class LiofTaManager extends HibernateUtil {
 			String where2 = "AND (d.name LIKE '" + name + "' AND d.food LIKE '"
 					+ food + "' AND d.drink LIKE '" + drink + "')";
 			String sql = select + " " + where1 + " " + where2;
-			result9Table = session.createSQLQuery(sql)
+			resultTable = session.createSQLQuery(sql)
 					.addEntity("LiofTa", LiofTa.class)
 					.addEntity("CoofTa", CoofTa.class).list();
 		} catch (Exception e) {
@@ -60,32 +55,7 @@ public class LiofTaManager extends HibernateUtil {
 			session.getTransaction().rollback();
 		}
 		session.getTransaction().commit();
-		this.outputTable = tableTrans(result9Table);
-		return outputTable;
+		return resultTable;
 	}
 
-	public ArrayList<Result9Table> tableTrans(List<?> result9Table) {
-		ArrayList<Result9Table> tempTable = new ArrayList<Result9Table>();
-		Object[] obj;
-		try {
-			for (int i = 0; i < result9Table.size(); i++) {
-				Result9Table temp = new Result9Table();
-				obj = (Object[]) result9Table.get(i);
-				LiofTa liofta = (LiofTa) obj[0];
-				CoofTa coofta = (CoofTa) obj[1];
-				temp.setId(liofta.getId());
-				temp.setName(liofta.getName());
-				temp.setFood(liofta.getFood());
-				temp.setDrink(liofta.getDrink());
-				temp.setColorNm(coofta.getColorNm());
-				temp.setTaste(coofta.getTaste());
-				tempTable.add(temp);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-
-		}
-
-		return tempTable;
-	}
 }
