94dfbd4dad2aa683fc4be3172dc875729b7dbe98
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-30 00:50:41 +0000

    佐藤　BandResultManager.javaを削除し、BandAllManager.javaにまとめる。 及びBandSearchAction.javaの修正に伴い他多数修正しました。

--- PC2015/src/action/BandSearchAction.java
+++ PC2015/src/action/BandSearchAction.java
@@ -6,101 +6,82 @@ import java.util.ArrayList;
 import model.BandResultTable;
 import model.BandTable;
 import controller.BandAllManager;
-import controller.BandResultManager;
 
-public class BandSearchAction extends AbstractAction{
+public class BandSearchAction extends AbstractAction {
 
 	private static final long serialVersionUID = 1L;
 
-	//フィールドの宣言
-	public String user_name;
-	public String band_name;
-	public String name;
-	public String part;
-	public String result;
-	public String do_print;
-	public String update_id; //String型からint型に修正→再びString型に戻す。
-	public String delete;
-	public String userId;
+	// フィールドの宣言
+	public String band_name; //バンド名の値を入れる変数
+	public String name; //名前の値を入れる変数
+	public String part; //演奏楽器の値を入れる変数
+	public String do_print; //検索結果の表示・非表示に関する変数
+	public String delete_id; //削除に関する変数
+	public String delete; //削除ボタンの表示・非表示に関する変数
+	public String userId; //ログイン時のUSERに関する変数
 
-	private BandResultManager linkController;
-	private BandAllManager allController;
-	public  ArrayList<BandResultTable> outputTable;
+	//
+	public ArrayList<BandResultTable> outputTable;
 
-	//getDefaultBand_nameメソッド。バンド名の値の初期化
-	private String getDefaultBand_name(){
-		this.band_name = "";
-		return this.band_name;
-	}
-	//getDefaultNameメソッド。名前の値の初期化
-	private String getDefaultName(){
-		this.name = "";
-		return this.name;
-	}
-	//getDefaultpartメソッド。演奏楽器の値の初期化
-	private String getDefaultPart(){
-		this.part = "";
-		return this.part;
+	// getDefaultValueメソッド。値の初期化
+	private String getDefaultValue() {
+		return "";
 	}
 
-	//executeメソッド。
+	// executeメソッド。
 	@Override
 	public String execute() {
-		this.userId = (String)this.sessionMap.get("userId");
-		//this.user_name = (String)this.sessionMap.get("user_name");
-		this.band_name = getDefaultBand_name();
-		this.name = getDefaultName();
-		this.part = getDefaultPart();
-		this.delete="faluse";
+		this.userId = (String) this.sessionMap.get("userId"); //USER名をuserIdに代入する
+		this.band_name = getDefaultValue(); //
+		this.name = getDefaultValue();
+		this.part = getDefaultValue();
+		this.delete = "false";
 		return "success";
 	}
-	//resetメソッド。入力した値を初期値に戻す。
-	public String reset(){
-		this.userId = (String)this.sessionMap.get("userId");
-		//this.user_name = (String)this.sessionMap.get("user_name");
-		this.band_name = getDefaultBand_name();
-		this.name = getDefaultName();
-		this.part = getDefaultPart();
+
+	// resetメソッド。入力した値を初期値に戻す。
+	public String reset() {
+		this.userId = (String) this.sessionMap.get("userId");
+		this.band_name = getDefaultValue();
+		this.name = getDefaultValue();
+		this.part = getDefaultValue();
 		return "success";
 	}
-	//printメソッド。検索結果を表示させるための処理？
-	public String print(){
 
-		/*this.band_name = checkcode(this.band_name);
-		this.name = checkcode(this.name);
-		this.part = checkcode(this.part);*/
+	// printメソッド。検索結果を表示させるための処理？
+	public String print() {
 
-		this.do_print = "true";
-		this.userId = (String)this.sessionMap.get("userId");
-		//this.user_name = (String)this.sessionMap.get("user_name");
-		if(this.band_name.isEmpty() && this.name.isEmpty() && this.part.isEmpty()){
-			//PC1007ならtryはスルーしている。これはスルーで来ていない。
-			try {
-				printall();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}else{
-			linkController  = new BandResultManager();
-			this.outputTable = linkController.bandResultList(this.band_name,this.name,this.part);
+		this.userId = (String) this.sessionMap.get("userId");
+		if (this.band_name.isEmpty() && this.name.isEmpty()
+				&& this.part.isEmpty()) {
+			BandAllManager allController = new BandAllManager();
+			this.outputTable = allController.bandResultList();
+		} else {
+			BandAllManager linkController = new BandAllManager();
+			this.outputTable = linkController.bandResultList(this.band_name,
+					this.name, this.part);
 		}
-		this.delete="true";
-		return "success";
-	}
-	//printallメソッド。
-	public String printall(){
 		this.do_print = "true";
-		this.userId = (String)this.sessionMap.get("userId");
-		//this.user_name = (String)this.sessionMap.get("user_name");
-		allController  = new BandAllManager();
-		this.outputTable = allController.resultList(this.band_name,this.name,this.part);
-		this.delete="true";
+		this.delete = "true";
 		return "success";
 	}
-	//updateメソッド。追加と削除で使用？
-	public String update(){
-		
-		this.sessionMap.put("update_id",this.update_id);
+
+	// updateメソッド。追加で使用
+	public String update() {
+		this.sessionMap.put("delete_id", null);
+		try {
+			this.response.sendRedirect("/PC2015/bandAdd.action");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return "success";
+	}
+
+	// deleteメソッド。行の削除に使用
+	public String delete() {
+
+		this.sessionMap.put("delete_id", this.delete_id);
 
 		try {
 			this.response.sendRedirect("/PC2015/bandAdd.action");
@@ -110,12 +91,5 @@ public class BandSearchAction extends AbstractAction{
 
 		return "success";
 	}
-	/*public String checkcode(String code) {
-		String r = "[a-zA-Z0-9]{0,50}";
-		if(code.matches(r)){
-			return code;
-		}
-		return "";
-	}*/
-	
+
 }
