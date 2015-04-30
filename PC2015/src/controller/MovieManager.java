d12a1c398c99ef91ed3ed31a5e86e46211ed0eb3
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-04-30 06:37:44 +0000

    tempの場所を移動しました。

--- PC2015/src/controller/MovieManager.java
+++ PC2015/src/controller/MovieManager.java
@@ -12,9 +12,8 @@ import org.hibernate.classic.Session;
 public class MovieManager extends HibernateUtil {
 
 	public  List<?> resultTableMovie;
-	public  ArrayList<ResultTableMovie> outputTableMovie;
 
-	public ArrayList<ResultTableMovie> resultList() {
+	public List<?> resultList() {
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
 
@@ -32,13 +31,10 @@ public class MovieManager extends HibernateUtil {
 			session.getTransaction().rollback();
 		}
 		session.getTransaction().commit();
-
-		this.outputTableMovie = tableTrans(resultTableMovie);
-
-		return outputTableMovie;
+		return resultTableMovie;
 	}
 	
-	public ArrayList<ResultTableMovie> resultList(String genreId,String exhibition_year){
+	public List<?> resultList(String genreId,String exhibition_year){
 
 		Session session = HibernateUtil.getSessionFactory()
 				.getCurrentSession();
@@ -59,38 +55,6 @@ public class MovieManager extends HibernateUtil {
 			session.getTransaction().rollback();
 		}
 		session.getTransaction().commit();
-		this.outputTableMovie = tableTrans(resultTableMovie);
-		return outputTableMovie;
-	}
-
-	
-	//表示結果の配列
-	public ArrayList<ResultTableMovie> tableTrans(List<?> resultTable){
-		ArrayList<ResultTableMovie> tempTable = new ArrayList<ResultTableMovie>();
-		Object[] obj;
-		try {
-			for(int i = 0 ; i < resultTable.size() ; i++){
-				ResultTableMovie temp = new ResultTableMovie();
-				obj = (Object[]) resultTable.get(i);
-				Movie movie =  (Movie)obj[0];
-				MovieGenre movie_genre  = (MovieGenre)obj[1];
-				temp.setGenre(movie_genre.getGenre());
-				temp.setId(movie.getId());
-				temp.setTitle(movie.getTitle());
-				temp.setExhibition_year(movie.getExhibition_year());
-//				temp.setRegistration_date(movie.getRegistration_date());
-//				temp.setRenewal_date(movie.getRenewal_date());
-//				temp.setRegistration_userid(movie.getRegistration_userid());
-//				temp.setRenewal_userid(movie.getRenewal_userid());
-//				temp.setControl(movie.getControl());
-//				temp.setDelete(movie.getDelete());
-				tempTable.add(temp);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-
-		}
-
-		return tempTable;
+		return resultTableMovie;
 	}
 }
