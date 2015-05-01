931cec69f4f7e018794898dba19fcce0ea600f54
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-05-01 09:28:32 +0000

    無駄な処理の削除し一つにまとめる、及び変更　佐藤

--- PC2015/src/controller/BandAllManager.java
+++ PC2015/src/controller/BandAllManager.java
@@ -13,8 +13,8 @@ import org.hibernate.classic.Session;
 public class BandAllManager extends HibernateUtil{
 	
 	//三つのメソッドで使用するため、フィールドで宣言
-	public List<?> bandResultTable;//いらない
-	public ArrayList<BandResultTable> outputTable;//いらない（BandSearchActionにまとめるから）
+	//public List<?> bandResultTable;//いらない
+	//public ArrayList<BandResultTable> outputTable;//いらない
 	
 	//bandResultListメソッド(引数なし)。検索時入力がなかったとき実行
 	public ArrayList<BandResultTable> bandResultList(){
@@ -31,6 +31,8 @@ public class BandAllManager extends HibernateUtil{
 		//select文とwhere文を合わせたものをsqlに代入
 		String sql = select + " " + where1;
 		
+		//SQL文の実行(固定文言)
+		List<?> bandResultTable = null; //SQLの検索結果用の変数
 		try{
 			bandResultTable = session.createSQLQuery(sql) //インスタンス生成
 					//SQLQuery.addEntityメソッドで戻り値BandAccountの型設定
@@ -41,43 +43,18 @@ public class BandAllManager extends HibernateUtil{
 			e.printStackTrace();
 			session.getTransaction().rollback();
 		}
+		
+		//トランザクションの終了(固定文言)
 		session.getTransaction().commit();
 		
-		this.outputTable = bandTableTrans(bandResultTable);
+		ArrayList<BandResultTable> outputTable = bandTableTrans(bandResultTable);
+		
+		//下の処理はもともとは必要だったもの。検索結果の処理を移すにあたり不要となる？
+		//this.outputTable = bandTableTrans(bandResultTable);
 		
 		return outputTable;
 	}
-	public ArrayList<BandResultTable> bandTableTrans(List<?> bandResultTable){
-		//ArrayList<BandResultTable>のインスタンス生成
-		ArrayList<BandResultTable> tempTable = new ArrayList<BandResultTable>();
-		Object[] obj;
-		try{
-			//for文でbandResultTableのサイズ分処理を繰り返す
-			for(int i = 0;i < bandResultTable.size();i++){
-				//BandResultTableのインスタンス生成
-				BandResultTable temp = new BandResultTable();
-				//bandResultTableのインデックスをすべて取得して、objectに代入
-				//リスト型を配列に変換(キャスト)する
-				obj = (Object[]) bandResultTable.get(i);
-				
-				BandAccount bandaccount = (BandAccount)obj[0];
-				BandTable bandtable = (BandTable)obj[1];
-				temp.setId(bandaccount.getId());
-				temp.setName(bandaccount.getName());
-				temp.setSex(bandaccount.getSex());
-				temp.setAge(bandaccount.getAge());
-				temp.setSchool(bandaccount.getSchool());
-				temp.setFavorite_song(bandaccount.getFavorite_song());
-				temp.setPart(bandaccount.getPart());
-				temp.setBand_name(bandtable.getBand_name());
-				tempTable.add(temp);
-			}
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		
-		return tempTable;
-	}
+	
 	
 	//BandResultManager.javaの処理をここに移す。bandResultListメソッド(引数あり)。検索時入力があったときに実行
 public ArrayList<BandResultTable> bandResultList(String band_name,String name,String part){
@@ -87,7 +64,8 @@ public ArrayList<BandResultTable> bandResultList(String band_name,String name,St
 				.getCurrentSession();
 		//トランザクション開始
 		session.beginTransaction();
-		try{
+		
+		
 			if(band_name.isEmpty()){band_name="%";} //band_nameが空の時はワイルドカード「%」を代入
 			if(name.isEmpty()){name="%";} //nameが空の時はワイルドカード「%」を代入
 			if(part.isEmpty()){part="%";} //partが空の時はワイルドカード「%」を代入
@@ -101,6 +79,9 @@ public ArrayList<BandResultTable> bandResultList(String band_name,String name,St
 			//sqlにselect文、where文二つを代入
 			String sql = select + " " + where1 + " " + where2;
 			
+			List<?> bandResultTable = null; //SQLの検索結果用の変数
+			try{
+				
 			bandResultTable = session.createSQLQuery(sql)
 					//SQLQuery.addEntityメソッドで戻り値BandAccountの型設定
 					.addEntity("BandAccount",BandAccount.class)
@@ -110,35 +91,52 @@ public ArrayList<BandResultTable> bandResultList(String band_name,String name,St
 			e.printStackTrace();
 			session.getTransaction().rollback();
 		}
+		
+		//トランザクションの終了(固定文言)	
 		session.getTransaction().commit();
-		this.outputTable = tableTrans(bandResultTable);
+		
+		ArrayList<BandResultTable> outputTable = bandTableTrans(bandResultTable);
+		//下の処理はもともとは必要だったもの。検索結果の処理を移すにあたり不要となる？
+		//this.outputTable = tableTrans(bandResultTable);
 		return outputTable;
 	}
-	//bandSearch.jspのiteratorを回すための処理。
-	public ArrayList<BandResultTable> tableTrans(List<?> bandResultTable){
+
+	//SQLの検索結果を画面表示用のListに入れ替えている
+	private ArrayList<BandResultTable> bandTableTrans(List<?> bandResultTable){
+		
+		//画面表示用のリストをインスタンス化
 		ArrayList<BandResultTable> tempTable = new ArrayList<BandResultTable>();
+		
+		//変数の初期化
 		Object[] obj;
-		try{
-			for(int i = 0;i < bandResultTable.size();i++){
-				BandResultTable temp = new BandResultTable();
-				obj = (Object[]) bandResultTable.get(i);
-				BandAccount bandaccount = (BandAccount)obj[0];
-				BandTable bandtable = (BandTable)obj[1];
-				temp.setId(bandaccount.getId());
-				temp.setName(bandaccount.getName());
-				temp.setSex(bandaccount.getSex());
-				temp.setAge(bandaccount.getAge());
-				temp.setSchool(bandaccount.getSchool());
-				temp.setFavorite_song(bandaccount.getFavorite_song());
-				temp.setPart(bandaccount.getPart());
-				temp.setBand_name(bandtable.getBand_name());
-				tempTable.add(temp);
+		
+		//SQLの検索結果の件数分ループ
+		for(int i = 0;i < bandResultTable.size();i++){
+			
+			//画面表示用のレコードをインスタンス化
+			BandResultTable temp = new BandResultTable();
+			
+			//SQLの検索結果を分解
+			obj = (Object[]) bandResultTable.get(i);
+			BandAccount bandaccount = (BandAccount)obj[0];
+			BandTable bandtable = (BandTable)obj[1];
+			
+			//画面表示用に設定
+			temp.setId(bandaccount.getId());
+			temp.setName(bandaccount.getName());
+			temp.setSex(bandaccount.getSex());
+			temp.setAge(bandaccount.getAge());
+			temp.setSchool(bandaccount.getSchool());
+			temp.setFavorite_song(bandaccount.getFavorite_song());
+			temp.setPart(bandaccount.getPart());
+			temp.setBand_name(bandtable.getBand_name());
+			
+			//画面表示用にリストに追加
+			tempTable.add(temp);
 			}
-		}catch(Exception e){
-			e.printStackTrace();
-		}
 		
 		return tempTable;
 	}
-
+	
+	
 }
