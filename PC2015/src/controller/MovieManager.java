e3fb2fb9361656f859acea3d762d5f14e9ac44dc
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-05-01 09:45:05 +0000

    コメントを追加しました。

--- PC2015/src/controller/MovieManager.java
+++ PC2015/src/controller/MovieManager.java
@@ -7,56 +7,65 @@ import model.MovieGenre;
 
 import org.hibernate.classic.Session;
 
-public class MovieManager extends HibernateUtil {//HibernateUtilを継承
+public class MovieManager extends HibernateUtil { // HibernateUtilを継承
 
-
-	//検索項目未入力の場合のメソッド
+	// 検索項目未入力の場合のメソッド
 	public List<?> resultList() {
-		  List<?> resultTableMovie = null;//リスト型の変数宣言
+		List<?> resultTableMovie = null; // リスト型の変数宣言
 
-		Session session = HibernateUtil.getSessionFactory().getCurrentSession();//DB接続
-		session.beginTransaction();//トランザクション(?)開始
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession(); // DB接続
+		session.beginTransaction(); // トランザクション(?)開始
 
-		//インサートするためのsql文
+		// インサートするためのsql文
 		String select = "SELECT * FROM movie m,movie_genre g";
 		String where1 = "WHERE m.genreId=g.id";
-		String sql    = select + " "+where1;
+		String sql = select + " " + where1;
 
 		try {
-			resultTableMovie = session.createSQLQuery(sql)//Queryインターフェイスのインスタンスを取得(?)
-					.addEntity("Movie", Movie.class)//ネイティブ SQL クエリからエンティティオブジェクトを取得する
-					.addEntity("MovieGenre", MovieGenre.class).list();//クエリの実行(?)
-		} catch (Exception e) {//例外がeに代入される
-			e.printStackTrace();
-			session.getTransaction().rollback();
-		}
-		session.getTransaction().commit();
-		return resultTableMovie;
-	}
-	
-	//検索項目入力済みの場合のメソッド
-	public List<?> resultList(String genreId,String exhibition_year){
-		  List<?> resultTableMovie = null;//リスト型の変数宣言
-
-		Session session = HibernateUtil.getSessionFactory()
-				.getCurrentSession();
-		session.beginTransaction();
-		try {
-			if(genreId.isEmpty())genreId="%";
-			if(exhibition_year.isEmpty())exhibition_year="%";
-			String select = "SELECT * FROM movie m,movie_genre g";
-			String where1 = "WHERE m.genreId=g.id";
-			String where2 = "AND (m.genreId LIKE '"+ genreId +"' AND m.exhibition_year LIKE '"
-					+ exhibition_year+ "')";
-			String sql = select + " " + where1 + " " + where2;
-			resultTableMovie = session.createSQLQuery(sql)
-					.addEntity("Movie", Movie.class)//ネイティブ SQL クエリからエンティティオブジェクトを取得する
-					.addEntity("MovieGenre", MovieGenre.class).list();//ネイティブ SQL クエリからエンティティオブジェクトを取得する
+			resultTableMovie = session.createSQLQuery(sql) // Queryインターフェイスのインスタンスを取得(?)
+					.addEntity("Movie", Movie.class) // ネイティブ SQLクエリからエンティティオブジェクトを取得する
+					.addEntity("MovieGenre", MovieGenre.class).list(); // クエリの実行(?)
+		 // 例外がeに代入される
 		} catch (Exception e) {
 			e.printStackTrace();
 			session.getTransaction().rollback();
 		}
-		session.getTransaction().commit();
+		session.getTransaction().commit(); // トランザクション(?)終了
+		return resultTableMovie;
+	}
+
+	// 検索項目入力済みの場合のメソッド
+	public List<?> resultList(String genreId, String exhibition_year) {
+		List<?> resultTableMovie = null;// リスト型の変数宣言
+
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();// DB接続
+		session.beginTransaction(); // トランザクション(?)開始
+		
+		try {
+			//　もしgenreIdが空だったらワイルドカード(%)を入れる
+			if (genreId.isEmpty()) {
+				genreId = "%";
+			}
+			//　もしexhibition_yearが空だったらワイルドカード(%)を入れる
+			if (exhibition_year.isEmpty()) {
+				exhibition_year = "%";
+			}
+			
+			// インサートするためのsql文
+			String select = "SELECT * FROM movie m,movie_genre g ";
+			String where1 = "WHERE m.genreId=g.id ";
+			String where2 = "AND (m.genreId LIKE '" + genreId
+					+ "' AND m.exhibition_year LIKE '" + exhibition_year + "')";
+			String sql = select + where1 +  where2;
+			resultTableMovie = session.createSQLQuery(sql) // Queryインターフェイスのインスタンスを取得(?)
+					.addEntity("Movie", Movie.class)// ネイティブ SQL。クエリからエンティティオブジェクトを取得する
+					.addEntity("MovieGenre", MovieGenre.class).list(); // クエリの実行(?)
+		 // 例外がeに代入される
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		}
+		session.getTransaction().commit(); // トランザクション(?)終了
 		return resultTableMovie;
 	}
 }
