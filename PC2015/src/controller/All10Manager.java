b9149c51a44588b53a54c3d2141310a1cbb3b416
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-05-01 04:49:37 +0000

    コメントの追加 横山

--- PC2015/src/controller/All10Manager.java
+++ PC2015/src/controller/All10Manager.java
@@ -7,14 +7,18 @@ import model.IDofEat;
 
 import org.hibernate.classic.Session;
 
-//HibernateUtilから継承されたAll10Managerクラス
+//HibernateUtilを継承したAll10Managerクラス
 public class All10Manager extends HibernateUtil{
+	
+	//複数個所で使われるためフィールドで宣言する
 	public List<?> result10Table;
 
+	//引数なしのresultList（検索欄入力なしの場合）
 	public List<?> resultList() {
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
 
+		//全件検索
 		String select = " SELECT * FROM eat_detail d,eat_id i";
 		String where1 = " WHERE d.id=i.id";
 		String sql = select + " " + where1;
@@ -22,7 +26,7 @@ public class All10Manager extends HibernateUtil{
 		try {
 			result10Table = session.createSQLQuery(sql)
 					.addEntity("eat_detail", DetailEat.class)
-					.addEntity("eat_id", IDofEat.class).list();  //テーブルを結合し、result10Tableにする。
+					.addEntity("eat_id", IDofEat.class).list();  //TODO(テーブルを結合し、result10Tableにする？)
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -33,7 +37,7 @@ public class All10Manager extends HibernateUtil{
 		return result10Table;
 	}
 	
-	
+	//引数ありのresultList（検索欄入力ありの場合）
 	public List<?> resultList(String eat_year, String eat_month,
 			String eat_day, String eat_hour) {
 		//データベースに接続する
@@ -49,16 +53,18 @@ public class All10Manager extends HibernateUtil{
 				eat_day = "%";  //eat_dayに入力された値が空の場合eat_dayはなんでも
 			if (eat_hour.isEmpty())
 				eat_hour = "%";  //eat_hourに入力された値が空の場合はeat_hourはなんでも
-			//テーブルの選択
+			//eat_detailとeat_idのテーブルを全件選択
 			String select = "SELECT * FROM eat_detail d, eat_id i";
 			//eat_detailのidとeat_idのidが同じ
 			String where1 = "WHERE d.id = i.id";
+			//入力された値とカラムの値が等しい
 			String where2 = "AND (d.eat_year LIKE '" + eat_year + "' AND d.eat_month LIKE '"
 					+ eat_month + "' AND d.eat_day LIKE '" + eat_day + "' AND d.eat_hour LIKE '" + eat_hour + "')";
 			String sql = select + " " + where1+ " " + where2;
 			result10Table = session.createSQLQuery(sql)
+					//
 					.addEntity("DetailEat", DetailEat.class)
-					.addEntity("IDofEat", IDofEat.class).list(); //TODO(2つのテーブルを一つのものにする？)
+					.addEntity("IDofEat", IDofEat.class).list(); //TODO（実行？）
 		} catch (Exception e) {
 			e.printStackTrace();
 			session.getTransaction().rollback(); //例外がキャッチされたらその前に戻る
