4aecc22cf294f0bb30c359a8b56397a2558c2680
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-05-01 03:54:51 +0000

    コメント追加しました。 横山

--- PC2015/src/action/Main10Action.java
+++ PC2015/src/action/Main10Action.java
@@ -13,58 +13,62 @@ public class Main10Action extends AbstractAction {
 
 	private static final long serialVersionUID = 1L;
 
-	//フィールドの宣言
-	public String eat_year;//年
-	public String eat_month;//月
-	public String eat_day;//日
-	public String eat_hour;//時間
-	public String do_search;//検索結果の表示・非表示フラグ
-	public String delete_id;//削除のチェックボックス
-	public String delete;//削除の表示・非表示フラグ
-	public String userId;//ログインのuserID
+	// フィールドの宣言
+	public String eat_year;// 年
+	public String eat_month;// 月
+	public String eat_day;// 日
+	public String eat_hour;// 時間
+	public String do_search;// 検索結果の表示・非表示フラグ
+	public String delete_id;// 削除のチェックボックス
+	public String delete;// 削除の表示・非表示フラグ
+	public String userId;// ログインのuserID
 
 	public ArrayList<Result10Table> outputTable;
+
 	// eat_yearのデフォルト値は2015
 	private String getDefaultEat_year() {
 		return "2015";
 	}
+
 	// eat_monthのデフォルト値は1
 	private String getDefaultEat_month() {
 		return "1";
 	}
+
 	// eat_dayのデフォルト値は1
 	private String getDefaultEat_day() {
 		return "1";
 	}
+
 	// eat_hourのデフォルト値はAM
 	private String getDefaultEat_hour() {
 		return "AM";
 	}
 
-	@Override //オーバーライドする
+	@Override
+	// オーバーライドする
 	// executeメソッドを作る（始めにデフォルト値を検索欄に表示させておくため）
-	public String execute() { 
+	public String execute() {
 		this.userId = (String) this.sessionMap.get("userId"); // sessionMapに保存していたuserIDを取得
 		this.eat_year = getDefaultEat_year(); // デフォルト値の取得
 		this.eat_month = getDefaultEat_month(); // デフォルト値の取得
 		this.eat_day = getDefaultEat_day(); // デフォルト値の取得
 		this.eat_hour = getDefaultEat_hour(); // デフォルト値の取得
 		this.delete = "false"; //
-		return "success"; //SuccessActionが見つからないので戻る
+		return "success"; // Successが見つからないのでMain10Actionへ戻る
 	}
-	
-	//resetメソッド（リセットボタンを押した時）
-	public String reset(){
+
+	// resetメソッド（リセットボタンを押した時）
+	public String reset() {
 		this.userId = (String) this.sessionMap.get("userId"); // sessionMapに保存していたuserIDを取得
 		this.eat_year = getDefaultEat_year(); // デフォルト値の取得
 		this.eat_month = getDefaultEat_month(); // デフォルト値の取得
 		this.eat_day = getDefaultEat_day(); // デフォルト値の取得
 		this.eat_hour = getDefaultEat_hour(); // デフォルト値の取得
-		return "success"; //SuccessActionが見つからないのでMain10Actionへ戻る
+		return "success"; // Successが見つからないのでMain10Actionへ戻る
 	}
-	
-	
-	//searchメソッド（検索ボタンを押した時）	
+
+	// searchメソッド（検索ボタンを押した時）
 	public String search() {
 		this.userId = (String) this.sessionMap.get("userId"); // sessionMapに保存していたuserIDを取得
 		All10Manager allController = new All10Manager(); // インスタンスの生成
@@ -72,21 +76,23 @@ public class Main10Action extends AbstractAction {
 		// もしすべて空だった場合は
 		if (this.eat_year.isEmpty() && this.eat_month.isEmpty()
 				&& this.eat_day.isEmpty() && this.eat_hour.isEmpty()) {
-			
+
 			result10Table = allController.resultList();
-		// 空でなかったら 検索結果の表示をする
+			// 空でなかったら 検索結果の表示をする
 		} else {
 			result10Table = allController.resultList(this.eat_year,
 					this.eat_month, this.eat_day, this.eat_hour);
 		}
 		this.outputTable = tableTrans(result10Table);
-		//検索結果の表示
+
+		// 検索結果の表示
 		this.do_search = "true";
-		//削除ボタンの表示
-		this.delete = "true"; 
-		return "success"; //SuccessActionが見つからないのでMain10Actionへ戻る
+		// 削除ボタンの表示
+		this.delete = "true";
+
+		return "success"; // Successが見つからないのでMain10Actionへ戻る
 	}
-	
+
 	// updateメソッド（追加ボタンを押したとき）
 	public String update() {
 		this.sessionMap.put("delete_id", null);
@@ -95,33 +101,37 @@ public class Main10Action extends AbstractAction {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return "success";  //SuccessActionが見つからないのでMain10Actionへ戻る
+		return "success"; // Successが見つからないのでMain10Actionへ戻る
 	}
 
 	// deleteメソッド（削除ボタンを押したとき）
 	public String delete() {
-		this.sessionMap.put("delete_id", this.delete_id);// update_idを取得する
+		this.sessionMap.put("delete_id", this.delete_id);// delete_idを取得する
 		try {
 			// 例外が発生するかもしれない処理
-			this.response.sendRedirect("/PC2015/update10.action"); // UpdateActionに接続する
-		} catch (IOException e) { 
+			this.response.sendRedirect("/PC2015/update10.action"); // Update10Actionに接続する
+		} catch (IOException e) {
 			// 例外をキャッチ
 			e.printStackTrace();
 		}
 
-		return "success";  //SuccessActionが見つからないのでMain10Actionへ戻る
+		return "success"; // SuccessActionが見つからないのでMain10Actionへ戻る
 	}
+
+	// TODO
 	public ArrayList<Result10Table> tableTrans(List<?> result10Table) {
 		ArrayList<Result10Table> tempTable = new ArrayList<Result10Table>();
 		Object[] obj;
 		try {
 			for (int i = 0; i < result10Table.size(); i++) {
+				// Result10Tableのインスタンスの生成
 				Result10Table temp = new Result10Table();
-				//Result10Tableのインスタンスの生成
+
 				obj = (Object[]) result10Table.get(i);
-				//結合したテーブルを表示させる
-				DetailEat detaileat = (DetailEat) obj[0];
-				IDofEat idofeat = (IDofEat) obj[1];
+
+				// 結合したテーブルを表示させる
+				DetailEat detaileat = (DetailEat) obj[0]; // 0番テーブル
+				IDofEat idofeat = (IDofEat) obj[1]; // 1番テーブル
 				temp.setId(detaileat.getId());
 				temp.setEat_year(detaileat.getEat_year());
 				temp.setEat_month(detaileat.getEat_month());
@@ -140,10 +150,9 @@ public class Main10Action extends AbstractAction {
 				tempTable.add(temp);
 			}
 		} catch (Exception e) {
-			e.printStackTrace(); //TODO（例外をキャッチする）
-
+			e.printStackTrace(); // TODO
 		}
 
-		return tempTable; //TODO
+		return tempTable; // TODO
 	}
 }
