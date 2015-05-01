3713b8aa42a4d7d76191c5d061d9e272ae4f24c0
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-05-01 06:18:51 +0000

    表示、非表示を変更しました。ほかり

--- PC2015/src/model/Genre.java
+++ PC2015/src/model/Genre.java
@@ -16,13 +16,13 @@ public class Genre implements Serializable{
 
 	private String id;
 	private String genreNm;
-	private String record_date;
-	private String reset_date;
-	private String entry_userId;
-	private String record_userId;
-	private String exclusive_FLG;
-	private String delete_FLG;
-	
+//	private String record_date;
+//	private String reset_date;
+//	private String entry_userId;
+//	private String record_userId;
+//	private String exclusive_FLG;
+//	private String delete_FLG;
+//	
 	/*--------------------------------------------------*/
 	@Id
 	@GeneratedValue
@@ -34,30 +34,30 @@ public class Genre implements Serializable{
 	public String getGenreNm() {
 		return genreNm;
 	}
-	@Column(name="record_date")
-	public String getRecord_date() {
-		return record_date;
-	}
-	@Column(name="reset_date")
-	public String getReset_date() {
-		return reset_date;
-	}
-	@Column(name="entry_userId")
-	public String getEntry_userId() {
-		return entry_userId;
-	}
-	@Column(name="record_userId")
-	public String getRecord_userId() {
-		return record_userId;
-	}
-	@Column(name="exclusive_FLG")
-	public String getExclusive_FLG() {
-		return exclusive_FLG;
-	}
-	@Column(name="delete_FLG")
-	public String getDelete_FLG() {
-		return delete_FLG;
-	}
+//	@Column(name="record_date")
+//	public String getRecord_date() {
+//		return record_date;
+//	}
+//	@Column(name="reset_date")
+//	public String getReset_date() {
+//		return reset_date;
+//	}
+//	@Column(name="entry_userId")
+//	public String getEntry_userId() {
+//		return entry_userId;
+//	}
+//	@Column(name="record_userId")
+//	public String getRecord_userId() {
+//		return record_userId;
+//	}
+//	@Column(name="exclusive_FLG")
+//	public String getExclusive_FLG() {
+//		return exclusive_FLG;
+//	}
+//	@Column(name="delete_FLG")
+//	public String getDelete_FLG() {
+//		return delete_FLG;
+//	}
 	
 /*--------------------------------------------------*/
 	public void setId(String id) {
@@ -66,22 +66,22 @@ public class Genre implements Serializable{
 	public void setGenreNm(String genreNm) {
 		this.genreNm = genreNm;
 	}
-	public void setRecord_date(String record_date) {
-		this.record_date = record_date;
-	}
-	public void setReset_date(String reset_date) {
-		this.reset_date = reset_date;
-	}
-	public void setEntry_userId(String entry_userId) {
-		this.entry_userId = entry_userId;
-	}
-	public void setRecord_userId(String record_userId) {
-		this.record_userId = record_userId;
-	}
-	public void setExclusive_FLG(String exclusive_FLG) {
-		this.exclusive_FLG = exclusive_FLG;
-	}
-	public void setDelete_FLG(String delete_FLG) {
-		this.delete_FLG = delete_FLG;
-	}
+//	public void setRecord_date(String record_date) {
+//		this.record_date = record_date;
+//	}
+//	public void setReset_date(String reset_date) {
+//		this.reset_date = reset_date;
+//	}
+//	public void setEntry_userId(String entry_userId) {
+//		this.entry_userId = entry_userId;
+//	}
+//	public void setRecord_userId(String record_userId) {
+//		this.record_userId = record_userId;
+//	}
+//	public void setExclusive_FLG(String exclusive_FLG) {
+//		this.exclusive_FLG = exclusive_FLG;
+//	}
+//	public void setDelete_FLG(String delete_FLG) {
+//		this.delete_FLG = delete_FLG;
+//	}
 }
