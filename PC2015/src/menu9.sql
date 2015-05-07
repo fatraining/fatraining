147b4d5548701c65e63e4de5c8144431d94747d9
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-05-07 02:31:05 +0000

    変更　江口

--- PC2015/src/menu9.sql
+++ PC2015/src/menu9.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2015 年 5 朁E07 日 04:04
+-- Generation Time: 2015 年 4 朁E27 日 07:37
 -- サーバのバージョン： 5.6.21
 -- PHP Version: 5.6.3
 
@@ -28,7 +28,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `table_color` (
 `id` int(50) NOT NULL,
-  `color` char(50) DEFAULT NULL,
+  `colorNm` char(50) DEFAULT NULL,
+  `taste` char(50) DEFAULT NULL,
   `day` char(50) DEFAULT NULL,
   `new_day` char(50) DEFAULT NULL,
   `userid` char(50) DEFAULT NULL,
@@ -41,12 +42,12 @@ CREATE TABLE IF NOT EXISTS `table_color` (
 -- テーブルのデータのダンプ `table_color`
 --
 
-INSERT INTO `table_color` (`id`, `color`, `day`, `new_day`, `userid`, `new_userid`, `time_stamp`, `delete`) VALUES
-(1, '赤', NULL, NULL, NULL, NULL, NULL, NULL),
-(2, '青', NULL, NULL, NULL, NULL, NULL, NULL),
-(3, '黄色', NULL, NULL, NULL, NULL, NULL, NULL),
-(4, 'オレンジ', NULL, NULL, NULL, NULL, NULL, NULL),
-(5, '緑', NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `table_color` (`id`, `colorNm`, `taste`, `day`, `new_day`, `userid`, `new_userid`, `time_stamp`, `delete`) VALUES
+(1, '赤', '映画鑑賞', NULL, NULL, NULL, NULL, NULL, NULL),
+(2, '青', '読書', NULL, NULL, NULL, NULL, NULL, NULL),
+(3, '黄色', '買い物', NULL, NULL, NULL, NULL, NULL, NULL),
+(4, 'オレンジ', 'カラオケ', NULL, NULL, NULL, NULL, NULL, NULL),
+(5, '緑', '散歩', NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -59,25 +60,24 @@ CREATE TABLE IF NOT EXISTS `table_like` (
   `name` char(50) DEFAULT NULL,
   `food` char(50) DEFAULT NULL,
   `drink` char(50) DEFAULT NULL,
-  `colorNm` int(50) DEFAULT NULL,
   `day` char(50) DEFAULT NULL,
   `new_day` char(50) DEFAULT NULL,
   `userid` char(50) DEFAULT NULL,
   `new_userid` char(50) DEFAULT NULL,
   `time_stamp` int(50) DEFAULT NULL,
   `delete` int(50) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- テーブルのデータのダンプ `table_like`
 --
 
-INSERT INTO `table_like` (`id`, `name`, `food`, `drink`, `colorNm`, `day`, `new_day`, `userid`, `new_userid`, `time_stamp`, `delete`) VALUES
-(1, '林', 'ピザ', 'コーラ', 4, NULL, NULL, NULL, NULL, NULL, NULL),
-(2, '瀬守', 'カレー', '水', 3, NULL, NULL, NULL, NULL, NULL, NULL),
-(3, '田中', 'サンドイッチ', 'オレンジジュース', 2, NULL, NULL, NULL, NULL, NULL, NULL),
-(4, '岡田', '煮物', 'お茶', 5, NULL, NULL, NULL, NULL, NULL, NULL),
-(5, '橘', 'ピザ', 'レモネード', 1, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `table_like` (`id`, `name`, `food`, `drink`, `day`, `new_day`, `userid`, `new_userid`, `time_stamp`, `delete`) VALUES
+(1, '林', 'ピザ', 'コーラ', NULL, NULL, NULL, NULL, NULL, NULL),
+(2, '瀬守', 'カレー', '水', NULL, NULL, NULL, NULL, NULL, NULL),
+(3, '田中', 'サンドイッチ', 'オレンジジュース', NULL, NULL, NULL, NULL, NULL, NULL),
+(4, '岡田', '煮物', 'お茶', NULL, NULL, NULL, NULL, NULL, NULL),
+(5, '橘', 'ピザ', 'レモネード', NULL, NULL, NULL, NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -108,7 +108,7 @@ MODIFY `id` int(50) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 -- AUTO_INCREMENT for table `table_like`
 --
 ALTER TABLE `table_like`
-MODIFY `id` int(50) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+MODIFY `id` int(50) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
