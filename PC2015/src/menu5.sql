8b5783d7b788dcade1f6f2d77ac8628cfba64c9a
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-05-07 09:45:11 +0000

    テーブル変更

--- PC2015/src/menu5.sql
+++ PC2015/src/menu5.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2015 年 4 朁E28 日 04:22
+-- Generation Time: 2015 年 5 朁E07 日 11:42
 -- サーバのバージョン： 5.6.21
 -- PHP Version: 5.6.3
 
@@ -34,15 +34,15 @@ CREATE TABLE IF NOT EXISTS `user_character` (
   `newday` char(50) DEFAULT NULL,
   `userid` char(50) DEFAULT NULL,
   `newuserid` char(50) DEFAULT NULL,
-  `delete` int(11) DEFAULT NULL,
+  `deleteFlg` int(11) DEFAULT NULL,
   `flg` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8;
 
 --
 -- テーブルのデータのダンプ `user_character`
 --
 
-INSERT INTO `user_character` (`id`, `personality`, `interest`, `day`, `newday`, `userid`, `newuserid`, `delete`, `flg`) VALUES
+INSERT INTO `user_character` (`id`, `personality`, `interest`, `day`, `newday`, `userid`, `newuserid`, `deleteFlg`, `flg`) VALUES
 (1, '優しい', '読書', NULL, NULL, NULL, NULL, NULL, NULL),
 (2, '明るい', 'カラオケ', NULL, NULL, NULL, NULL, NULL, NULL),
 (3, '穏やか', 'ゲーム', NULL, NULL, NULL, NULL, NULL, NULL);
@@ -54,28 +54,28 @@ INSERT INTO `user_character` (`id`, `personality`, `interest`, `day`, `newday`, 
 --
 
 CREATE TABLE IF NOT EXISTS `user_profile` (
-  `id` int(11) DEFAULT NULL,
+`id` int(11) NOT NULL,
   `phonenumber` int(11) DEFAULT NULL,
   `zipcode` int(11) DEFAULT NULL,
   `dwelling` char(50) DEFAULT NULL,
   `name` char(50) DEFAULT NULL,
-`personality2` int(11) NOT NULL,
+  `personality2` int(11) NOT NULL,
   `day` char(50) DEFAULT NULL,
   `newday` char(50) DEFAULT NULL,
   `userid` char(50) DEFAULT NULL,
   `newuserid` char(50) DEFAULT NULL,
-  `delete` int(11) DEFAULT NULL,
+  `deleteFlg` int(11) DEFAULT NULL,
   `flg` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- テーブルのデータのダンプ `user_profile`
 --
 
-INSERT INTO `user_profile` (`id`, `phonenumber`, `zipcode`, `dwelling`, `name`, `personality2`, `day`, `newday`, `userid`, `newuserid`, `delete`, `flg`) VALUES
+INSERT INTO `user_profile` (`id`, `phonenumber`, `zipcode`, `dwelling`, `name`, `personality2`, `day`, `newday`, `userid`, `newuserid`, `deleteFlg`, `flg`) VALUES
 (1, 2, 3, '千葉', '田中', 1, NULL, NULL, NULL, NULL, NULL, NULL),
-(3, 3, 1, '埼玉', '大木', 2, NULL, NULL, NULL, NULL, NULL, NULL),
-(2, 1, 2, '東京', '佐藤', 3, NULL, NULL, NULL, NULL, NULL, NULL);
+(2, 1, 2, '東京', '佐藤', 3, NULL, NULL, NULL, NULL, NULL, NULL),
+(3, 3, 1, '埼玉', '大木', 2, NULL, NULL, NULL, NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -91,7 +91,7 @@ ALTER TABLE `user_character`
 -- Indexes for table `user_profile`
 --
 ALTER TABLE `user_profile`
- ADD PRIMARY KEY (`personality2`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -101,12 +101,12 @@ ALTER TABLE `user_profile`
 -- AUTO_INCREMENT for table `user_character`
 --
 ALTER TABLE `user_character`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=58;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=51;
 --
 -- AUTO_INCREMENT for table `user_profile`
 --
 ALTER TABLE `user_profile`
-MODIFY `personality2` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
