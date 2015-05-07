dfa434bdc2951df359f1f0cb1ad314078d055398
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-05-07 02:30:16 +0000

    変更　江口

--- PC2015/src/action/Update9Action.java
+++ PC2015/src/action/Update9Action.java
@@ -12,22 +12,23 @@ import org.hibernate.HibernateException;
 import org.hibernate.classic.Session;
 
 import controller.HibernateUtil;
-import controller.LiofTaManager;
 
 @Result(name = "main9", value = "main9.action", type = ServletRedirectResult.class)
 public class Update9Action extends AbstractAction {
 	private static final long serialVersionUID = 1L;
-	// 名前
 	public String name;
-	// 食べ物
 	public String food;
-	// 飲み物
 	public String drink;
-	// 色
-	public String color;
-
-	// 変数
+	public String colorNm;
+	public String taste;
+	public String day;
+	public String new_day;
+	public String userid;
+	public String new_userid;
 	public String delete_id;
+	// public int time_stamp;
+	// public int delete;
+	public String errormsg;
 
 	// executeメソッド
 	public String execute() throws Exception {
@@ -37,66 +38,52 @@ public class Update9Action extends AbstractAction {
 
 	// insertメソッド（挿入）
 	public String insert() {
-		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-		session.beginTransaction();
-		
 		// 日付
 		Date date = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd k:m:s");
-		String day = String.valueOf(sdf.format(date));
-		String new_day = String.valueOf(sdf.format(date));
+		day = String.valueOf(sdf.format(date));
+		new_day = String.valueOf(sdf.format(date));
+		// USER
+		this.userid = (String) this.sessionMap.get("userId");
+		this.new_userid = (String) this.sessionMap.get("userId");
 
-		// USERid
-		String userid = (String) this.sessionMap.get("userId");
-		String new_userid = (String) this.sessionMap.get("userId");
-		
-		CoofTa insert_color_table = new CoofTa();
-		
-		insert_color_table.setColor(this.color);
-		insert_color_table.setDay(day);
-		insert_color_table.setNew_day(new_day);
-		insert_color_table.setUserid(userid);
-		insert_color_table.setNew_userid(new_userid);
-		
-		try {
-			session.save(insert_color_table);
-		} catch (HibernateException e) {
-			e.printStackTrace();
-			session.getTransaction().rollback();
-		}
-		
-		LiofTaManager lioftamanager = new LiofTaManager();
-		insert_color_table = lioftamanager.cooftaList();
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
 
 		LiofTa insert_like_table = new LiofTa();
-		
+		CoofTa insert_color_table = new CoofTa();
+
 		insert_like_table.setName(this.name);
 		insert_like_table.setFood(this.food);
 		insert_like_table.setDrink(this.drink);
-		insert_like_table.setColorNm(insert_color_table.getId());
-		insert_like_table.setDay(day);
-		insert_like_table.setNew_day(new_day);
-		insert_like_table.setUserid(userid);
-		insert_like_table.setNew_userid(new_userid);
+		insert_color_table.setColorNm(this.colorNm);
+		insert_color_table.setTaste(this.taste);
+		insert_like_table.setDay(this.day);
+		insert_like_table.setNew_day(this.new_day);
+		insert_like_table.setUserid(this.userid);
+		insert_like_table.setNew_userid(this.new_userid);
 
+		// 例外処理
 		try {
 			session.save(insert_like_table);
+			session.save(insert_color_table);
+
 		} catch (HibernateException e) {
 			e.printStackTrace();
 			session.getTransaction().rollback();
 		}
+		
+		session.getTransaction().commit(); // 処理が成功したときに結果を確立させる
+		return "main9";
 
-		session.getTransaction().commit();
-
-		return "main9";	
 	}
 
-	// deleteメソッド（消去）
+	// deleteメソッド
 	public String delete() {
 		this.delete_id = (String) this.sessionMap.get("delete_id");
 		String str = new String(this.delete_id);
 		String[] strAry = str.split(",");
-
+		
 		if (this.delete_id.isEmpty()) {
 			return "main9";
 		}
