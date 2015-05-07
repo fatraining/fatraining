64daa21bd1c38eb54862d1d237e646bda2cbf537
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-05-07 05:23:02 +0000

    変更　江口

--- PC2015/src/action/Update9Action.java
+++ PC2015/src/action/Update9Action.java
@@ -12,6 +12,7 @@ import org.hibernate.HibernateException;
 import org.hibernate.classic.Session;
 
 import controller.HibernateUtil;
+import controller.LiofTaManager;
 
 @Result(name = "main9", value = "main9.action", type = ServletRedirectResult.class)
 public class Update9Action extends AbstractAction {
@@ -19,16 +20,15 @@ public class Update9Action extends AbstractAction {
 	public String name;
 	public String food;
 	public String drink;
+	public String color;
+
 	public String colorNm;
-	public String taste;
+
 	public String day;
 	public String new_day;
 	public String userid;
 	public String new_userid;
 	public String delete_id;
-	// public int time_stamp;
-	// public int delete;
-	public String errormsg;
 
 	// executeメソッド
 	public String execute() throws Exception {
@@ -38,42 +38,59 @@ public class Update9Action extends AbstractAction {
 
 	// insertメソッド（挿入）
 	public String insert() {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		
+		String day;
+		String new_day;
+		
 		// 日付
 		Date date = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd k:m:s");
 		day = String.valueOf(sdf.format(date));
 		new_day = String.valueOf(sdf.format(date));
+		
+		String userid;
+		String new_userid;
+		
 		// USER
-		this.userid = (String) this.sessionMap.get("userId");
-		this.new_userid = (String) this.sessionMap.get("userId");
+		userid = (String) this.sessionMap.get("userId");
+		new_userid = (String) this.sessionMap.get("userId");
 
-		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-		session.beginTransaction();
-
-		LiofTa insert_like_table = new LiofTa();
 		CoofTa insert_color_table = new CoofTa();
-
-		insert_like_table.setName(this.name);
-		insert_like_table.setFood(this.food);
-		insert_like_table.setDrink(this.drink);
 		insert_color_table.setColorNm(this.colorNm);
-		insert_color_table.setTaste(this.taste);
-		insert_like_table.setDay(this.day);
-		insert_like_table.setNew_day(this.new_day);
-		insert_like_table.setUserid(this.userid);
-		insert_like_table.setNew_userid(this.new_userid);
 
-		// 例外処理
 		try {
-			session.save(insert_like_table);
 			session.save(insert_color_table);
 
 		} catch (HibernateException e) {
 			e.printStackTrace();
 			session.getTransaction().rollback();
 		}
-		
-		session.getTransaction().commit(); // 処理が成功したときに結果を確立させる
+
+		LiofTaManager lioftamanager = new LiofTaManager();
+		insert_color_table = lioftamanager.cooftaList();
+
+		LiofTa insert_like_table = new LiofTa();
+
+		insert_like_table.setColor(insert_color_table.getId());
+		insert_like_table.setName(this.name);
+		insert_like_table.setFood(this.food);
+		insert_like_table.setDrink(this.drink);
+		insert_like_table.setDay(day);
+		insert_like_table.setNew_day(new_day);
+		insert_like_table.setUserid(userid);
+		insert_like_table.setNew_userid(new_userid);
+
+		try {
+			session.save(insert_like_table);
+
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		}
+
+		session.getTransaction().commit();
 		return "main9";
 
 	}
@@ -81,9 +98,10 @@ public class Update9Action extends AbstractAction {
 	// deleteメソッド
 	public String delete() {
 		this.delete_id = (String) this.sessionMap.get("delete_id");
+		
 		String str = new String(this.delete_id);
 		String[] strAry = str.split(",");
-		
+
 		if (this.delete_id.isEmpty()) {
 			return "main9";
 		}
@@ -95,15 +113,16 @@ public class Update9Action extends AbstractAction {
 
 			session.beginTransaction();
 			try {
-				CoofTa coofta = (CoofTa) session.load(CoofTa.class, strAry[i]);
 				LiofTa liofta = (LiofTa) session.load(LiofTa.class, strAry[i]);
+				CoofTa coofta = (CoofTa) session.load(CoofTa.class,
+						liofta.getColor());
 				session.delete(coofta);
 				session.delete(liofta);
 			} catch (HibernateException e) {
 				e.printStackTrace();
-				session.getTransaction().rollback(); // 障害が起こった時その前の状態まで戻る
+				session.getTransaction().rollback();
 			}
-			session.getTransaction().commit(); // 処理が成功したときに結果を確立させる
+			session.getTransaction().commit();
 		}
 		return "main9";
 	}
