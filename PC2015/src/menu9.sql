0f93790a31a20b8bb69cab7cb1a37fedd93c5d15
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-05-08 08:23:16 +0000

    変更　江口

--- PC2015/src/menu9.sql
+++ PC2015/src/menu9.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2015 年 5 朁E08 日 03:33
+-- Generation Time: 2015 年 5 朁E08 日 10:06
 -- サーバのバージョン： 5.6.21
 -- PHP Version: 5.6.3
 
@@ -35,18 +35,18 @@ CREATE TABLE IF NOT EXISTS `table_color` (
   `new_userid` char(50) DEFAULT NULL,
   `time_stamp` int(50) DEFAULT NULL,
   `delete` int(50) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- テーブルのデータのダンプ `table_color`
 --
 
 INSERT INTO `table_color` (`id`, `colorNm`, `day`, `new_day`, `userid`, `new_userid`, `time_stamp`, `delete`) VALUES
-(1, '赤', NULL, NULL, NULL, NULL, NULL, NULL),
-(2, '青', NULL, NULL, NULL, NULL, NULL, NULL),
-(3, '黄色', NULL, NULL, NULL, NULL, NULL, NULL),
-(4, 'オレンジ', NULL, NULL, NULL, NULL, NULL, NULL),
-(5, '緑', NULL, NULL, NULL, NULL, NULL, NULL);
+(1, '赤', '3 / 23', '3 / 24', 'Struts2', 'Struts2', 0, 0),
+(2, '青', '4 / 15', '4 / 16', 'Struts2', 'Struts2', 0, 0),
+(3, '黄色', '5 / 9', '5 / 10', 'Struts2', 'Struts2', 0, 0),
+(4, 'オレンジ', '7 / 30', '7 / 31', 'Struts2', 'Struts2', 0, 0),
+(5, '緑', '10 / 24', '10 / 25', 'Struts2', 'Struts2', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -66,18 +66,18 @@ CREATE TABLE IF NOT EXISTS `table_like` (
   `new_userid` char(50) DEFAULT NULL,
   `time_stamp` int(50) DEFAULT NULL,
   `delete` int(50) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- テーブルのデータのダンプ `table_like`
 --
 
 INSERT INTO `table_like` (`id`, `name`, `food`, `drink`, `color`, `day`, `new_day`, `userid`, `new_userid`, `time_stamp`, `delete`) VALUES
-(1, '林', 'ピザ', 'コーラ', 4, NULL, NULL, NULL, NULL, NULL, NULL),
-(2, '瀬守', 'カレー', '水', 3, NULL, NULL, NULL, NULL, NULL, NULL),
-(3, '田中', 'サンドイッチ', 'オレンジジュース', 1, NULL, NULL, NULL, NULL, NULL, NULL),
-(4, '岡田', '煮物', 'お茶', 5, NULL, NULL, NULL, NULL, NULL, NULL),
-(5, '橘', 'ピザ', 'レモネード', 2, NULL, NULL, NULL, NULL, NULL, NULL);
+(1, '原田', 'ピザ', 'コーラ', 4, '7 / 30', '7 / 31', 'Struts2', 'Struts2', 0, 0),
+(2, '瀬守', 'カレー', '水', 3, '5 / 9', '5 / 10', 'Struts2', 'Struts2', 0, 0),
+(3, '田中', 'サンドイッチ', 'オレンジジュース', 1, '3 / 23', '3 / 24', 'Struts2', 'Struts2', 0, 0),
+(4, '岡田', '煮物', 'お茶', 5, '10 / 24', '10 / 25', 'Struts2', 'Struts2', 0, 0),
+(5, '橘', 'ピザ', 'レモネード', 2, '4 / 15', '4 / 16', 'Struts2', 'Struts2', 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -103,12 +103,12 @@ ALTER TABLE `table_like`
 -- AUTO_INCREMENT for table `table_color`
 --
 ALTER TABLE `table_color`
-MODIFY `id` int(50) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+MODIFY `id` int(50) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `table_like`
 --
 ALTER TABLE `table_like`
-MODIFY `id` int(50) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+MODIFY `id` int(50) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
