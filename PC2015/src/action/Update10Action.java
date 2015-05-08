aff832ff98609443aee2ad1cb84e5dde8b885ea9
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-05-08 02:31:37 +0000

    UpdateActionのdeleteメソッドをManagerに移動しました。横山

--- PC2015/src/action/Update10Action.java
+++ PC2015/src/action/Update10Action.java
@@ -1,19 +1,10 @@
 package action;
 
-//import java.io.UnsupportedEncodingException;
-
-import model.DetailEat;
 import model.IDofEat; //モデルのインポート
 
 import org.apache.struts2.config.Result;
 import org.apache.struts2.dispatcher.ServletRedirectResult;
-import org.hibernate.HibernateException;
-import org.hibernate.classic.Session;
 
-import java.text.SimpleDateFormat;
-import java.util.*;
-
-import controller.HibernateUtil;
 import controller.All10Manager;
 
 @Result(name = "main10", value = "main10.action", type = ServletRedirectResult.class)
@@ -60,46 +51,11 @@ public class Update10Action extends AbstractAction {
 	public String delete() {
 		this.delete_id = (String) this.sessionMap.get("delete_id");// セッションマップからdelete_idを取得
 
-		// 複数選択の削除のために文字列の分割
-		String str = new String(this.delete_id);
-		String[] strAry = str.split(",");
-
-		// delete_idが空である場合
-		if (this.delete_id.isEmpty()) {
-			return "main10"; // Main10Actionへ
-		}
-
-		// for文で処理を繰り返す
-		for (int i = 0; i < strAry.length; i++) {
-
-			// データベースに接続
-			Session session = HibernateUtil.getSessionFactory()
-					.getCurrentSession();
-			// トランザクションの開始
-			session.beginTransaction();
-			try {
-				DetailEat detaileat = (DetailEat) session.load(DetailEat.class,
-						strAry[i]);
-				IDofEat idofeat = (IDofEat) session.load(IDofEat.class,
-						detaileat.getFood_id());
-
-				// DetailEat detaileat = (DetailEat)
-				// session.load(DetailEat.class,
-				// Integer.valueOf(delete_id));
-				// IDofEat idofeat = (IDofEat) session.load(IDofEat.class,
-				// Integer.valueOf(detaileat.getFood_id())); //
-				// TODO(delete_idがStringでは処理できないのでキャストする)
-
-				session.delete(detaileat); // detail_tableの指定した行を削除する
-				session.delete(idofeat); // id_tableの指定した行を削除する
-
-			} catch (HibernateException e) {
-				e.printStackTrace();
-				session.getTransaction().rollback();
-			}
-			// トランザクションの終了
-			session.getTransaction().commit();
-		}
+All10Manager delete = new All10Manager();
+		
+		//insertメソッドを参照
+		delete.delete(delete_id);
+		
 		return "main10"; // Main10Actionへ
 	}
 
