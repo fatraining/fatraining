608f51283e7f84b1403cc927a419a0456b776bda
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-05-08 07:06:01 +0000

    レコードの内容修正

--- PC2015/src/menu1.sql
+++ PC2015/src/menu1.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2015 年 5 朁E07 日 04:27
+-- Generation Time: 2015 年 5 朁E08 日 08:54
 -- サーバのバージョン： 5.6.21
 -- PHP Version: 5.6.3
 
@@ -35,16 +35,15 @@ CREATE TABLE IF NOT EXISTS `table_hobby` (
   `new_userid` char(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `time_stamp` int(11) DEFAULT NULL,
   `delete` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- テーブルのデータのダンプ `table_hobby`
 --
 
 INSERT INTO `table_hobby` (`id`, `hobby`, `day`, `new_day`, `userid`, `new_userid`, `time_stamp`, `delete`) VALUES
-(1, '書道', NULL, NULL, NULL, NULL, NULL, NULL),
-(2, 'サッカー', NULL, NULL, NULL, NULL, NULL, NULL),
-(4, 'サッカー', NULL, NULL, NULL, NULL, NULL, NULL);
+(1, '書道', '12/04', '01/01', 'aaa', 'bbb', 0, 0),
+(2, 'サッカー', '07/08', '10/27', 'kkk', 'ooo', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -65,16 +64,15 @@ CREATE TABLE IF NOT EXISTS `table_profile` (
   `new_userid` char(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `time_stamp` int(11) DEFAULT NULL,
   `delete` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- テーブルのデータのダンプ `table_profile`
 --
 
 INSERT INTO `table_profile` (`id`, `name`, `personality`, `home`, `birthday`, `hobby_id`, `day`, `new_day`, `userid`, `new_userid`, `time_stamp`, `delete`) VALUES
-(1, '真斗', '大人しい', '京都', 12, 1, NULL, NULL, NULL, NULL, NULL, NULL),
-(2, '音也', '元気', '東京', 4, 2, NULL, NULL, NULL, NULL, NULL, NULL),
-(4, '翔', '男気', '名古屋', 6, 4, '2015/05/07 9:58:31', '2015/05/07 9:58:31', 'Struts2', 'Struts2', NULL, NULL);
+(1, '真斗', '大人しい', '京都', 12, 1, '03/04', '09/24', 'uuu', 'fff', 0, 0),
+(2, '音也', '元気', '東京', 4, 2, '11/15', '03/07', 'ddd', 'ttt', 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -100,12 +98,12 @@ ALTER TABLE `table_profile`
 -- AUTO_INCREMENT for table `table_hobby`
 --
 ALTER TABLE `table_hobby`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `table_profile`
 --
 ALTER TABLE `table_profile`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
