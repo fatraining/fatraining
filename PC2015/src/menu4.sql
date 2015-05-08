9ed814362e37a5e19b23c7d051952facb632e5e3
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-05-08 03:29:56 +0000

    データベースのband_accountとband_tableの整理　佐藤

--- PC2015/src/menu4.sql
+++ PC2015/src/menu4.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2015 年 4 朁E23 日 07:44
+-- Generation Time: 2015 年 5 朁E08 日 05:23
 -- サーバのバージョン： 5.6.21
 -- PHP Version: 5.6.3
 
@@ -41,18 +41,16 @@ CREATE TABLE IF NOT EXISTS `band_account` (
   `RENEWAL_USERID` varchar(50) DEFAULT NULL,
   `EXCLUSION_FLG` int(50) DEFAULT NULL,
   `DELETE_FLG` int(50) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- テーブルのデータのダンプ `band_account`
 --
 
 INSERT INTO `band_account` (`ID`, `NAME`, `SEX`, `AGE`, `SCHOOL`, `FAVORITE_SONG`, `PART`, `BAND_ID`, `ENTRY_DATE`, `RENEWAL_DATE`, `ENTRY_USERID`, `RENEWAL_USERID`, `EXCLUSION_FLG`, `DELETE_FLG`) VALUES
-(1, '佐藤大樹', '男', '22', '城西大学', '二月の兵隊', 'ギター', 1, 'value-9', 'value-10', 'value-11', 'value-12', 0, 0),
-(2, '山田', '男', '20', '銀座第23高校', '君が代', 'コーラス', 2, 'value-9', 'value-10', 'value-11', 'value-12', 0, 0),
-(5, 'ダビデ', '男', '99', '美術館', 'レクイエム', 'ピアノ', 5, '2015/04/22 14:34:4', '2015/04/22 14:34:4', NULL, NULL, 0, 0),
-(10, 'ｖｖｖｖ', 'あああ', '21', 'da', 'fs', 'fvs', 10, '2015/04/22 19:42:44', '2015/04/22 19:42:44', 'Struts2', 'Struts2', 0, 0),
-(11, '田中さん', '女', '29', 'モンスターがハンターな学校', '戦闘曲B', '双剣', 11, '2015/04/23 10:26:28', '2015/04/23 10:26:28', 'tanaka', 'tanaka', 0, 0);
+(1, '佐藤利夫', '男', '26', '早稲田大学', 'ストレンジカメレオン', 'ギター', 1, '2015/05/08 12:15:10', '2015/05/08 12:15:10', 'sato', 'sato', 0, 0),
+(2, '山田太郎', '男', '17', '銀座第3高等学校', '鮮やかな殺人', 'ベース', 2, '2015/05/08 12:17:26', '2015/05/08 12:17:26', 'sato', 'sato', 0, 0),
+(3, '木下キリン', '女', '15', 'さいたま芸術学校', 'Love/Hate', 'ドラム', 3, '2015/05/08 12:19:58', '2015/05/08 12:19:58', 'sato', 'sato', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -69,19 +67,16 @@ CREATE TABLE IF NOT EXISTS `band_table` (
   `RENEWAL_USERID` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `EXCLUSION_FLG` int(50) DEFAULT NULL,
   `DELETE_FLG` int(50) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- テーブルのデータのダンプ `band_table`
 --
 
 INSERT INTO `band_table` (`ID`, `BAND_NAME`, `ENTRY_DATE`, `RENEWAL_DATE`, `ENTRY_USERID`, `RENEWAL_USERID`, `EXCLUSION_FLG`, `DELETE_FLG`) VALUES
-(1, '砂糖と塩', 'value-3', 'value-4', 'value-5', 'value-6', 0, 0),
-(2, '時雨', '[value-3]', '[value-4]', '[value-5]', '[value-6]', 0, 0),
-(3, 'da', NULL, NULL, NULL, NULL, NULL, NULL),
-(5, '時雨', NULL, NULL, NULL, NULL, NULL, NULL),
-(10, 'ｖｖｖｖ', NULL, NULL, NULL, NULL, NULL, NULL),
-(11, 'こんがり焼けました', NULL, NULL, NULL, NULL, NULL, NULL);
+(1, '砂糖と塩', '2015/05/08 12:15:10', '2015/05/08 12:15:10', 'sato', 'sato', 0, 0),
+(2, '時雨', '2015/05/08 12:17:26', '2015/05/08 12:17:26', 'sato', 'sato', 0, 0),
+(3, '芸術学校', '2015/05/08 12:19:58', '2015/05/08 12:19:58', 'sato', 'sato', 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -107,12 +102,12 @@ ALTER TABLE `band_table`
 -- AUTO_INCREMENT for table `band_account`
 --
 ALTER TABLE `band_account`
-MODIFY `ID` int(50) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+MODIFY `ID` int(50) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `band_table`
 --
 ALTER TABLE `band_table`
-MODIFY `ID` int(50) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+MODIFY `ID` int(50) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
