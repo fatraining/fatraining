6f72ef71ad6533c36fd4d9119b00332e48956006
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-05-08 02:18:09 +0000

    deleteメソッドの処理の移動　佐藤

--- PC2015/src/action/BandAddAction.java
+++ PC2015/src/action/BandAddAction.java
@@ -1,17 +1,8 @@
 package action;
 
-import java.util.*;
-import java.text.SimpleDateFormat;
-
-import model.BandAccount;
-import model.BandTable;
-
 import org.apache.struts2.config.Result;
 import org.apache.struts2.dispatcher.ServletRedirectResult;
-import org.hibernate.HibernateException;
-import org.hibernate.classic.Session;
 
-import controller.HibernateUtil;
 import controller.BandAllManager;
 
 @Result(name = "bandsearch", value = "bandSearch.action", type = ServletRedirectResult.class)
@@ -26,14 +17,7 @@ public class BandAddAction extends AbstractAction {
 	public String school; // 出身校に関する変数schoolを宣言
 	public String favorite_song; // 好きな曲に関する変数favorite_songを宣言
 	public String part; // 演奏楽器に関する変数partを宣言
-	// public int exclusion_flg; //更新FLGに関する変数exclusion_flgを宣言
-	// public int delete_flg; //削除FLGに関する変数delete_flgを宣言
 	public String band_name; // バンド名に関する変数band_nameを宣言
-
-	// public String entry_userid;
-	// public String renewal_userid;
-	// public String entry_date;
-	// public String renewal_date;
 
 	// executeメソッド。update_idの値を受け取っている。
 	public String execute() throws Exception {
@@ -49,10 +33,10 @@ public class BandAddAction extends AbstractAction {
 		// 更新useridにuserIdの値を入れる。
 		String renewal_userid = (String) this.sessionMap.get("userId");
 
-		//BandAllManagerのインスタンス生成
+		// BandAllManagerのインスタンス生成
 		BandAllManager insert = new BandAllManager();
 
-		//BandAllManagerのinsertメソッドを参照
+		// BandAllManagerのinsertメソッドを参照
 		insert.insert(this.band_name, this.name, this.sex, this.age,
 				this.school, this.favorite_song, this.part, entry_userid,
 				renewal_userid);
@@ -61,43 +45,18 @@ public class BandAddAction extends AbstractAction {
 
 	}
 
-	/********************** 複数選択の削除の処理ここから **************************************/
-
 	// deleteメソッド。削除するときの処理
 	public String delete() {
 		this.delete_id = (String) this.sessionMap.get("delete_id");
 
-		// 複数選択の削除のために文字列の分割
-		String str = new String(this.delete_id);
-		String[] strAry = str.split(",");
+		// BandAllManagerのインスタンス生成
+		BandAllManager delete = new BandAllManager();
 
-		if (this.delete_id.isEmpty()) {
-			return "bandsearch";
-		}
-		// for文で処理を繰り返す
-		for (int i = 0; i < strAry.length; i++) {
-			// データベースに接続
-			Session session = HibernateUtil.getSessionFactory()
-					.getCurrentSession();
-			// トランザクションを開始
-			session.beginTransaction();
-			try {
-				BandAccount bandaccount = (BandAccount) session.load(
-						BandAccount.class, strAry[i]);
-				BandTable bandtable = (BandTable) session.load(BandTable.class,
-						bandaccount.getBand_id());
-				session.delete(bandaccount); // band_accountテーブルの選択された行を削除
-				session.delete(bandtable); // band_tableテーブルの選択された行を削除
-			} catch (HibernateException e) {
-				e.printStackTrace();
-				session.getTransaction().rollback();
-			}
-			session.getTransaction().commit();//
-		}
+		// BandAllManagerのdeleteメソッドを参照
+		delete.delete(delete_id);
+
 		return "bandsearch";
 
 	}
-
-	/************************** 複数選択の削除の処理ここまで *******************************/
 
 }
