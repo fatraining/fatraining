373f8ff337b1c6a4a12d2b4f99c7f8319320a171
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-05-13 09:13:41 +0000

    エラーメッセージの追加　ほかり

--- PC2015/src/action/Main2Action.java
+++ PC2015/src/action/Main2Action.java
@@ -13,115 +13,148 @@ public class Main2Action extends AbstractAction {
 
 	private static final long serialVersionUID = 1L;
 
-	//入力項目
-	public String name; //お菓子の名前:
-	public String genreNm; //ジャンル:
-	public String delete_id; //削除チェックボックス
-	
-	//検索結果の表示(データ)
+	// 入力項目
+	public String name; // お菓子の名前:
+	public String genreNm; // ジャンル:
+	public String delete_id; // 削除チェックボックス
+	public String errormsg;
+
+	// 検索結果の表示(データ)
 	public ArrayList<Result2Table> outputTable;
-	
-	//検索結果の表示フラグ
+
+	// 検索結果の表示フラグ
 	public String do_search;
-	
-    //削除の表示フラグ
+
+	// 削除の表示フラグ
 	public String delete;
-	
+
 	public String add_id;
 
-	//画面が表示時に実行
+	// 画面が表示時に実行
 	@Override
 	public String execute() {
 
-		//初期値の設定
+		// 初期値の設定
 		this.name = getDefaultName();
 		this.genreNm = getDefaultGenreNm();
-		
+
 		return "success";
 	}
-	
-	//初期値の設定（お菓子の名前）
+
+	// 初期値の設定（お菓子の名前）
 	private String getDefaultName() {
 		return "";
 	}
 
-	//初期値の設定（ジャンル）
+	// 初期値の設定（ジャンル）
 	private String getDefaultGenreNm() {
 		return "";
 	}
-	
-	//リセットボタンを押下時
+
+	// リセットボタンを押下時
 	public String reset() {
-		//初期値に戻す
+		// 初期値に戻す
 		this.name = getDefaultName();
 		this.genreNm = getDefaultGenreNm();
 		return "success";
 	}
 
-	
-	
-	
-	
-	//追加ボタンを押下時
+	// 追加ボタンを押下時
 	public String add() {
-		
-		//削除チェックボックスの値を取得しセッション（add_id）に設定
-		this.sessionMap.put("delete_id",null);
-		
-		//削除画面に遷移
+
+		// 削除チェックボックスの値を取得しセッション（add_id）に設定
+		this.sessionMap.put("delete_id", null);
+
+		// 削除画面に遷移
 		try {
 			this.response.sendRedirect("/PC2015/update2.action");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		return "success";		
+
+		return "success";
 	}
 
-
-	//検索ボタンを押下時
+	// 検索ボタンを押下時
 	public String search() {
+		this.sessionMap.put("name", this.name);
+		this.sessionMap.put("genreNm", this.genreNm);
 
-		//SQLの実行
+		// SQLの実行
 		List<?> resultTable = null;
 		if (this.name.isEmpty() && this.genreNm.isEmpty()) {
-			//入力がない場合
+			// 入力がない場合
 			SweetsManager allController = new SweetsManager();
 			resultTable = allController.searchAll();
-		}else{
-			//入力された場合
+		} else {
+			// 入力された場合
 			SweetsManager linkController = new SweetsManager();
 			resultTable = linkController.searchAll(this.genreNm, this.name);
 		}
 		this.outputTable = tableTrans(resultTable);
-		
-		
-		//検索結果の表示
+
+		// 検索結果の表示
 		this.do_search = "true";
-		//削除ボタンの表示
+		// 削除ボタンの表示
 		this.delete = "true";
-		
+
 		return "success";
 	}
 	
-	//削除ボタンを押下時
-	public String delete() {
-		
-		//削除チェックボックスの値を取得しセッション（delete_id）に設定
-		this.sessionMap.put("delete_id",this.delete_id);
-		
-		//削除画面に遷移
-		try {
-			this.response.sendRedirect("/PC2015/update2.action");
-		} catch (IOException e) {
-			e.printStackTrace();
+	public String insert() {
+		if (this.delete_id.isEmpty()) {
+			this.errormsg = "１つ以上項目をチェックしてください";
+			
 		}
-		
-		return "success";		
+		return "errormsg";
 	}
-	 
-	
-	
+
+	// 削除ボタンを押下時
+	public String delete() {
+
+		// 削除チェックボックスの値を取得しセッション（delete_id）に設定
+		this.sessionMap.put("delete_id", this.delete_id);
+		
+
+
+		if (this.delete_id == null) {
+
+			// SQLの実行
+			
+			this.name = (String) this.sessionMap.get("name");
+			this.genreNm = (String) this.sessionMap.get("genreNm");
+
+			List<?> resultTable = null;
+			if (this.name.isEmpty() && this.genreNm.isEmpty()) {
+				// 入力がない場合
+				SweetsManager allController = new SweetsManager();
+				resultTable = allController.searchAll();
+			} else {
+				// 入力された場合
+				SweetsManager linkController = new SweetsManager();
+				resultTable = linkController.searchAll(this.genreNm, this.name);
+			}
+			this.outputTable = tableTrans(resultTable);
+
+			// 検索結果の表示
+			this.do_search = "true";
+			// 削除ボタンの表示
+			this.delete = "true";
+			this.errormsg = "true";
+
+			return "success";
+		}
+
+			// 削除画面に遷移
+			try {
+				this.response.sendRedirect("/PC2015/update2.action");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			return "success";
+		}
+
 	// SQLの検索結果を画面表示用のListに入れ替えている
 	private ArrayList<Result2Table> tableTrans(List<?> resultTable) {
 
