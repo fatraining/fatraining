1aec6f9012a80e301d102a9db7aaff2b7170a98c
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2015-09-18 09:06:07 +0000

    修正しました　武政

--- PC2015/src/action/MenuAction.java
+++ PC2015/src/action/MenuAction.java
@@ -6,22 +6,21 @@ import org.apache.struts2.config.Results;
 import org.apache.struts2.dispatcher.ServletRedirectResult;
 
 @Results({
-	@Result(name = "main1", value = "main1.action", type = ServletRedirectResult.class),
-	@Result(name = "main2", value = "main2.action", type = ServletRedirectResult.class),
-	@Result(name = "main3", value = "main3.action", type = ServletRedirectResult.class),
-	@Result(name = "main4", value = "bandSearch.action", type = ServletRedirectResult.class),
-	@Result(name = "main5", value = "main5.action", type = ServletRedirectResult.class),
-	@Result(name = "main6", value = "main6.action", type = ServletRedirectResult.class),
-	@Result(name = "main7", value = "main7.action", type = ServletRedirectResult.class),
-	@Result(name = "yasaiSearch", value = "yasaiSearch.action", type = ServletRedirectResult.class),
-	@Result(name = "main9", value = "main9.action", type = ServletRedirectResult.class),
-	@Result(name = "main10", value = "main10.action", type = ServletRedirectResult.class)
-})
-
+		@Result(name = "main1", value = "main1.action", type = ServletRedirectResult.class),
+		@Result(name = "main2", value = "main2.action", type = ServletRedirectResult.class),
+		@Result(name = "main3", value = "main3.action", type = ServletRedirectResult.class),
+		@Result(name = "main4", value = "bandSearch.action", type = ServletRedirectResult.class),
+		@Result(name = "main5", value = "main5.action", type = ServletRedirectResult.class),
+		@Result(name = "main6", value = "main6.action", type = ServletRedirectResult.class),
+		@Result(name = "main7", value = "main7.action", type = ServletRedirectResult.class),
+		@Result(name = "yasaiSearch", value = "yasaiSearch.action", type = ServletRedirectResult.class),
+		@Result(name = "main9", value = "main9.action", type = ServletRedirectResult.class),
+		@Result(name = "main10", value = "main10.action", type = ServletRedirectResult.class),
+		@Result(name = "kaisyaSearch", value = "kaisyaSearch.action", type = ServletRedirectResult.class) })
 public class MenuAction extends AbstractAction {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	public String execute() throws Exception {
 		return "sccess";
 	}
@@ -33,7 +32,7 @@ public class MenuAction extends AbstractAction {
 	public String gotoMain2() {
 		return "main2";
 	}
-	
+
 	public String gotoMain3() {
 		return "main3";
 	}
@@ -41,7 +40,7 @@ public class MenuAction extends AbstractAction {
 	public String gotoMain4() {
 		return "main4";
 	}
-	
+
 	public String gotoMain5() {
 		return "main5";
 	}
@@ -49,7 +48,7 @@ public class MenuAction extends AbstractAction {
 	public String gotoMain6() {
 		return "main6";
 	}
-	
+
 	public String gotoMain7() {
 		return "main7";
 	}
@@ -57,12 +56,16 @@ public class MenuAction extends AbstractAction {
 	public String gotoMain8() {
 		return "yasaiSearch";
 	}
-	
+
 	public String gotoMain9() {
 		return "main9";
 	}
 
 	public String gotoMain10() {
 		return "main10";
+	}
+
+	public String gotoStaff() {
+		return "kaisyaSearch";
 	}
 }
