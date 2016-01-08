6ce36c5d0603fa94e5b8eaf8224f0691faf7e6e1
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2016-01-08 08:57:20 +0000

    ValidateUtilのクラス名を変更

---
+++ PC2015/src/util/ValidateUtil.java
@@ -0,0 +1,45 @@
+package util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * バリデーションユーティリティやで
+ * @author miyamoto
+ *
+ */
+public class ValidateUtil {
+
+	/**
+	 * 引数のemailが<br>
+	 * ○○@○○.○○の形になっているかチェックするやで。
+	 * @return boolean
+	 */
+	public static boolean emailValidate(String email){
+		String str = email;
+		String regex = "[\\w\\.\\-]+@(?:[\\w\\-]+\\.)+[\\w\\-]+";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(str);
+		if(m.find()){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	/**
+	 * 引数のinputTimeがHH:mmの形式になっているかチェックするやで。
+	 * @param inputTime
+	 * @return boolean
+	 */
+	public static boolean inputTimeValidate(String inputTime){
+		String regex = "^([0-1][0-9]|2[0-3]):[0-5][0-9]$";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(inputTime);
+		if(m.find()){
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
