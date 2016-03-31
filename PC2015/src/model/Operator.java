ec1c72330f1c4c481c3657db21b9ae8e9e7e77ae
ogawa <ogawa@9235FDD6-0195-11E4-AC2B-2776A437689A>
2016-03-31 01:29:18 +0000

    SQLとの通信制御とモデル追加

---
+++ PC2015/src/model/Operator.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@SuppressWarnings("unused")
+@Entity
+@Table(name = "operator")
+public class Operator implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private  String operatorID;
+	private  String operatorName;
+	private String userID;
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "operatorID")
+	public String getOperatorID() {
+		return operatorID;
+	}
+	@Column(name = "operatorName")
+	public  String getOperatorName() {
+		this.operatorName = operatorName.trim();
+		return operatorName;
+	}
+
+	public void setOperatorID(String operatorID) {
+		this.operatorID = operatorID;
+	}
+	public void setOperatorName(String operatorName) {
+		this.operatorName = operatorName;
+	}
+	
+}
