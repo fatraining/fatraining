29b9c94c381d41b5d49d9610669a189d5e4854c5
fa_training <fa_training@9235FDD6-0195-11E4-AC2B-2776A437689A>
2016-05-06 06:05:39 +0000

    背景スライド操作js追加

---
+++ WebAppTraining/WebContent/assets/js/yamavegas.js
@@ -0,0 +1,16 @@
+/**
+ * スルァイド用背景画像
+ */
+$('body').vegas({
+  overlay: true,
+  transition: 'burn',
+  transitionDuration: 2000,
+  delay: 10000,
+  animation: 'random',
+  animationDuration: 20000,
+  slides: [
+    { src: 'assets/images/001.jpg' },
+    { src: 'assets/images/002.jpg' },
+    { src: 'assets/images/003.jpg' },
+  ]
+});
