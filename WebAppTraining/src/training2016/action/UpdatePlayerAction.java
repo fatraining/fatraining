907c36589e0a7994a8731f2314683641cb800b79
fa_training <fa_training@9235FDD6-0195-11E4-AC2B-2776A437689A>
2016-05-06 06:13:44 +0000

    追加画面Action変更

--- WebAppTraining/src/training2016/action/UpdatePlayerAction.java
+++ WebAppTraining/src/training2016/action/UpdatePlayerAction.java
@@ -1,10 +1,12 @@
 package training2016.action;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.struts2.config.Result;
 import org.apache.struts2.dispatcher.ServletRedirectResult;
@@ -32,12 +34,16 @@ public class UpdatePlayerAction extends AbstractAction {
 	private String age;
 	/** コメント */
 	private String comment;
+	/** ファイル */
+	 private File img;
+	 private String imgContentType;
+	 private String imgFileName;
 	/** 削除ID */
 	private String delete;
 	/** チーム名プルダウン用マップ */
 	private Map<String, String> teamMap;
-	/** ポジションプルダウン用リスト */
-	private List<String> positionList;
+	/** ポジションプルダウン用マップ */
+	private Map<String,String> positionMap = new LinkedHashMap<String, String>();
 
 	/** エラーメッセージ */
 	private String errorMsg;
@@ -54,7 +60,7 @@ public class UpdatePlayerAction extends AbstractAction {
 		// deleteを取り出し
 		this.setDelete((String)this.sessionMap.get("deleteID"));
 		this.setTeamMap();
-		this.setPositionList(positionList);
+		this.setPositionMap();
 		return "success";
 	}
 
@@ -66,19 +72,30 @@ public class UpdatePlayerAction extends AbstractAction {
 	 */
 	public String insert() {
 		this.setTeamMap();
-		this.setPositionList(positionList);
+		this.setPositionMap();
 
-	// 未入力の項目があるときにエラーを返す
-	if (!this.isValidate()) {
-		this.errorMsg = "必須項目(※)を入力してください。";
-		return "error";
-	}
+		// ファイル入力があれば、/images/temp にコピー
+		if (this.img != null) {
+			setImgFileName(String.valueOf(System.currentTimeMillis()) + ".jpg");
+			File destFile = new File("D:\\pleiades\\workspace\\WebAppTraining\\WebContent\\assets\\images\\temp\\" + getImgFileName());
+			try {
+				FileUtils.copyFile(img, destFile);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 
-	PlayerDao dao = new PlayerDao();
-	dao.save(this.generatePlayersModel());
-	this.msg = "登録しました";
-	sessionMap.put("msg", this.msg);
-	return "search";
+		// 未入力の項目があるときにエラーを返す
+		if (!this.isValidate()) {
+			this.errorMsg = "必須項目(※)を入力してください。";
+			return "error";
+		}
+
+		PlayerDao dao = new PlayerDao();
+		dao.save(this.generatePlayersModel());
+		this.msg = "登録しました";
+		sessionMap.put("msg", this.msg);
+		return "search";
 	}
 
 	/**
@@ -95,6 +112,7 @@ public class UpdatePlayerAction extends AbstractAction {
 		model.setPosition(this.position);
 		model.setAge(Integer.parseInt(this.age));
 		model.setComment(this.comment);
+		model.setImagePath(this.imgFileName);
 
 		return model;
 	}
@@ -131,8 +149,8 @@ public class UpdatePlayerAction extends AbstractAction {
 	// チーム名プルダウン用マップを返す //
 	/*------------------------------------------------------*/
 	public Map<String, String> tableTrans(List<?> resultTable) {
-		Map<String, String> tempMap = new HashMap<String, String>();
-		tempMap.put("", "");
+		Map<String, String> tempMap = new LinkedHashMap<String, String>();
+		tempMap.put("", "チームを選択");
 		try {
 			for (int i = 0; i < resultTable.size(); i++) {
 				Team team = (Team) resultTable.get(i);
@@ -156,18 +174,21 @@ public class UpdatePlayerAction extends AbstractAction {
 	}
 
 	/**
-	 * @return positionList
+	 * @return positionMap
 	 */
-	public List<String> getPositionList() {
-		return positionList;
+	public Map<String, String> getPositionMap() {
+		return positionMap;
 	}
 
 	/**
-	 * @param positionList
-	 *            セットする positionList
+	 * @param positionMap セットする positionMap
 	 */
-	public void setPositionList(List<String> positionList) {
-		this.positionList = Arrays.asList("", "FW", "MF", "DF", "GK");
+	public void setPositionMap() {
+		this.positionMap.put("","ポジションを選択");
+		this.positionMap.put("FW","FW");
+		this.positionMap.put("MF","MF");
+		this.positionMap.put("DF","DF");
+		this.positionMap.put("GK","GK");
 	}
 
 	/**
@@ -295,4 +316,28 @@ public class UpdatePlayerAction extends AbstractAction {
 	public String getErrorMsg() {
 		return this.errorMsg;
 	}
+
+	public File getImg() {
+		return img;
+	}
+
+	public void setImg(File img) {
+		this.img = img;
+	}
+
+	public String getImgFileName() {
+		return imgFileName;
+	}
+	public void setImgFileName(String imgFileName) {
+		this.imgFileName = imgFileName;
+	}
+
+	public String getImgContentType() {
+		return imgContentType;
+	}
+
+	public void setImgContentType(String imgContentType) {
+		this.imgContentType = imgContentType;
+	}
+
 }
