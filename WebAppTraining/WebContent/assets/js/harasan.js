2a052313c81e851550bcd3f660c7eb1b8850a184
fa_training <fa_training@9235FDD6-0195-11E4-AC2B-2776A437689A>
2016-05-12 07:29:37 +0000

    更新ボタン押下→ブラウザの戻るボタンで戻る→もう一度更新ボタン押下で落ちるバグを修正

--- WebAppTraining/WebContent/assets/js/harasan.js
+++ WebAppTraining/WebContent/assets/js/harasan.js
@@ -65,9 +65,10 @@ $(function(){
 	// 更新ボタン用
 	$('[name=updateBtn]').click(function (e) {
 		// パラメータ用エレメントを作成
-		var param = document.createElement('input');
+		param = document.createElement('input');
 		param.setAttribute('type', 'hidden');
 		param.setAttribute('name', 'updateId');
+		param.setAttribute('id',   'updateId');
 		param.setAttribute('value', $(this).attr('forUpdate'));
 
 		// update メソッドを実行するための要素を生成
@@ -77,6 +78,11 @@ $(function(){
 		btn.setAttribute('value', "1");
 
 		var form = document.forms[0];
+		// 前に作ったパラメータ用エレメントあったら消す（戻るボタン対策）
+		var oldParam = document.getElementById('updateId');
+		if (oldParam != undefined && oldParam != null) {
+			form.removeChild(oldParam);
+		}
 		form.appendChild(param);
 		form.appendChild(btn);
 		form.submit();
