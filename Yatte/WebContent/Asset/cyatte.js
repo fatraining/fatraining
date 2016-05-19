f9e4bd7995958c2d9654da247c3ba4ec5bf14614
fa_training <fa_training@9235FDD6-0195-11E4-AC2B-2776A437689A>
2016-05-19 05:50:31 +0000

    おなじくcyatte

---
+++ Yatte/WebContent/Asset/cyatte.js
@@ -0,0 +1,175 @@
+//開いたらサーバー接続させる
+//サーバーサイドでセッションとってくれる
+var uri = "ws://" + location.host + "/Yatte/cyo";
+ws = new WebSocket(uri);
+
+// 使用するとき
+// var msg = new yatteMessage("yatte", new yatteList);
+var yatteMessage = function (messageType, contentsList) {
+	this.contentsList = new Array();
+	this.contentsList.push(contentsList);
+};
+
+/* 送信用スクリプト */
+// 格納させるやーつ JSONってやつ
+var yatteList = {
+	name : null,
+	who : "who",
+	num : "num",
+	time : "time",
+	ex : "",
+	yattal : null,
+	flg : 1,
+}
+
+
+// カラム隠しておく！！つくっておく！
+// Columnsっていうライブラリ
+$('.popbox').hide();
+$('#columns').hide();
+$(document).ready(function() {
+	$('#columns').columns({
+		data : ["",""]
+	});
+});
+
+
+/*******************************************************************************
+ * yatte押された時の処理 ついでに選択肢の初期化 サーバーで処理させる場合、$('# option:selected').val()で指定
+ ******************************************************************************/
+$( '#ex' ).keypress( function ( e ) {
+	if ( e.which == 13 ) {
+		$("#yatte").click();
+		return true;
+	}
+} );
+
+$("#yatte").click(function() {
+	var m = $('#name').text()
+	var w = $('#who option:selected').text()
+	var t = null
+	var e = $('#ex').val()
+
+	console.log(m)
+	if (e != "" ) {
+	var yatteList = {
+		name : m,
+		who : w,
+		time : t,
+		ex : e + "@" + m,
+		flg : 0,
+		}
+	ws.send(JSON.stringify(yatteList));
+	console.log("送る方 : " + JSON.stringify(yatteList))
+
+	$('#who').val(0);
+	$('#num').val(0);
+	$('#ex').val("");
+	target = document.getElementById("error")
+	target.innerHTML  = "";
+	} else {
+		target = document.getElementById("error")
+		target.innerHTML  = "なにをすればいいのやら";
+	}
+});
+
+/*
+ * yattalの処理
+ */
+function yattal(no) {
+	console.log("やったる！" + no );
+	var yatteList = {
+			no : no,
+			flg : 1,
+	};
+
+	ws.send(JSON.stringify(yatteList));
+}
+
+/* 受信用スクリプト */
+ws.onmessage = function(message) {
+	/* 表示制御 */
+
+	$('.popbox').show();
+	$('#columns').show();
+	var focus = document.getElementById('#ex');
+/* console.log("受け取った方 : " + message.data); */
+
+	/* message.dataをJSON形式にparse */
+	var json = JSON.parse(message.data);
+	console.log(JSON.stringify(json))
+	if (json.length == 0) {
+		$('#columns').hide();
+	} else {
+	json.reverse()
+
+	/* カラム作成(の前にデストローイ) */
+	$('#columns').columns('destroy');
+
+
+	$('#columns').columns({
+		data : json,
+		schema: [
+		         {"header":"No", "key":"no"},
+		         {"header":"やるひと！", "key":"who"},
+		         {"header":"いつまで！", "key":"time"},
+		         {"header":"こめんと！", "key":"ex"},
+		         {"header":"yattal !", "key":"yattal",
+		        	 },
+// {"header":"yattal !", "key":"time",
+// "template":'<font color="FF0000"><strong>あかんかった</strong></font>',
+// "condition":function(time) {return (time != 0);},},
+		         ]
+	});
+
+	$('#receive').text(json);
+
+
+
+
+
+/* デスクトップ通知 */
+/*
+ * Notifications API で処理 Firefox,Chrome,Opera,Vivaldiとかいける(IEぶっころ)
+ * 意外と簡単に通知が実装できるやーつ 通知のbodyには\n使えるよ！！
+ */
+    // Notificationを取得
+	data : json.reverse()
+	if(json.getJSONArray(0).getString("flg") === 0){
+    var Notification = window.Notification || window.mozNotification || window.webkitNotification;
+
+    // Notificationの権限チェック
+    Notification.requestPermission(function (permission) {
+        // console.log(permission);
+    });
+
+    // 通知インスタンス生成
+    var instance = new Notification(
+        "【 from : " + json[json.length-1]['name']
+        + "】 ", // 通知タイトル
+        {body : "to : " + json[json.length-1]['who']
+        		+ "\n[" + json[json.length-1]['ex'] + "]",// 通知内容
+        	icon : 'images/yatte.jpg'
+        }
+    );
+
+    setTimeout(instance.close.bind(instance),5000); // 1000[ミリ秒後]に通知を閉じる
+
+    instance.onclick = function () {
+    	window.focus(); // ←なんかしらんけど使える(Chrome)
+    	window.opener.focus(); // ←なんかしらんけど使えない(多分Firefox用)
+        console.log("onclick");
+    };
+    instance.onerror = function () {
+        console.log("onerror");
+    };
+    instance.onshow = function () {
+    	window.focus(); // ←なんかしらんけど使えない
+        console.log("onshow");
+    };
+    instance.onclose = function () {
+        console.log("onclose");
+    };
+	}
+	}
+};
---
+++ Yatte/WebContent/cyatteLogin.jsp
@@ -0,0 +1,48 @@
+<%@ page session="true"%>
+<%@page import="endpoint.yatteName"%>
+<%@page import="java.util.ArrayList"%>
+<%@page import="com.mysql.fabric.xmlrpc.base.Array"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css"
+	href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" />
+<style>
+div.header {
+	max-width: 1000px;
+	margin-left: 30%;
+}
+
+p.err {
+	text-align: center;
+	color: red;
+}
+</style>
+<body>
+	<form action="/Yatte/cyatte" method="POST">
+		<p class="err"></p>
+		<div class="form-group">
+			<div class="col-sm-offset-2 col-sm-2">
+				<label>わいねーむ :</label>
+			</div>
+			<div class="col-sm-2">
+				<select name="name" id="name" class="form-control">
+					<%
+						ArrayList<yatteName> yList = (ArrayList<yatteName>) request.getAttribute("nameList");
+						for (int i = 0; i < yList.size(); i++) {
+							out.print("<option ID='" + yList.get(i).getName() + "'>");
+							out.print(yList.get(i).getName());
+							out.print("</option>");
+						}
+					%>
+				</select>
+			</div>
+		</div>
+		<div class="col-sm-2">
+			<input type="submit" value="LOGIN" class="btn btn-warning" />
+		</div>
+	</form>
+</body>
+</html>
+
