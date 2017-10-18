8ad0cf8d15216cb91577944e8914effefd2e0040
fa_training <fa_training@9235FDD6-0195-11E4-AC2B-2776A437689A>
2017-10-18 03:25:44 +0000

    小川さんは優しい

---
+++ WebAppTraining/src/training2016/action/OgasanYasashi.java
@@ -0,0 +1,12 @@
+package training2016.action;
+
+public class OgasanYasashi {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("作ったのは辻です");
+
+		System.out.println("小川さんは優しい");
+	}
+
+}
