b9abe5cf029a3c9bb0b38ec4382753e2bedf492e
fatraining <40687688+fatraining@users.noreply.github.com>
2019-05-28 14:09:56 +0900

    修正したものをコミット致します（種市）

--- web-app-training/src/main/java/jp/co/futureantiques/webapptraining/model/form/bandTaneichi/BandTaneichiInputForm.java
+++ web-app-training/src/main/java/jp/co/futureantiques/webapptraining/model/form/bandTaneichi/BandTaneichiInputForm.java
@@ -5,27 +5,27 @@ import jp.co.futureantiques.webapptraining.model.bandTaneichi.BandMainTaneichi;
 public class BandTaneichiInputForm {
 
 	public void initWithBandMainTaneichi(BandMainTaneichi bandMainTaneichi) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO 時間があればつくる
 
 	}
 
 	public BandMainTaneichi convertToBandMainTaneichiForInsert() {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO 時間があればつくる
 		return null;
 	}
 
 	public long getId() {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO 時間があればつくる
 		return 0;
 	}
 
 	public Object getUpdateDate() {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO 時間があればつくる
 		return null;
 	}
 
 	public BandMainTaneichi convertToBandMainTaneichiForUpdate(BandMainTaneichi bandMainTaneichi) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO 時間があればつくる
 		return null;
 	}
 
