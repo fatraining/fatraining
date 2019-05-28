4b6a7086627e8d9be93d2857d6697867291cfda8
fatraining <40687688+fatraining@users.noreply.github.com>
2019-05-28 10:28:41 +0900

    お城のSpecification作成しました

---
+++ web-app-training/src/main/java/jp/co/futureantiques/webapptraining/repository/specification/CastleMinamiSpecification.java
@@ -0,0 +1,103 @@
+package jp.co.futureantiques.webapptraining.repository.specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.thymeleaf.util.StringUtils;
+
+import jp.co.futureantiques.webapptraining.constant.CommonConst;
+import jp.co.futureantiques.webapptraining.model.castleMinami.CastleMainMinami;
+import jp.co.futureantiques.webapptraining.model.form.castleMinami.CastleSearchForm;
+
+/**
+ * CastleMainの検索条件を生成するクラス
+ *
+ * @author Minami
+ *
+ */
+public class CastleMinamiSpecification {
+
+	/**
+	 * 検索条件生成の実装
+	 *
+	 * @param CastleSearchForm
+	 * @return CastleMainのSpecification
+	 */
+	public static Specification<CastleMainMinami> generateCastleMinamiSpecification(final CastleSearchForm form){
+		return new Specification<CastleMainMinami>() {
+			@Override
+			public Predicate toPredicate(Root<CastleMainMinami> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+
+				//検索条件
+				Predicate condition = null;
+
+				//削除フラグON用が1かどうか判定
+				if(form.getIsDelete() == 1) {
+
+					//削除フラグ=1を検索条件にする
+					return cb.equal(root.get("delFlg"), CommonConst.DELETE_FLG_ON);
+				}
+
+				//条件が入力されている場合条件追加
+				if(form.getId() !=null) {
+
+					//IDを条件に追加
+					Predicate newCondition = cb.equal(root.get("id"), form.getId());
+					condition = getPredicate(cb, condition, newCondition);
+				}
+
+				if(!StringUtils.isEmpty(form.getCastleName())) {
+
+					//お城名を条件に追加
+					form.setCastleName(form.getCastleName().trim());
+					Predicate newCondition = cb.like(root.get("castleName"), "%" + form.getCastleName() + "%");
+					condition = getPredicate(cb, condition,newCondition);
+				}
+
+				if(form.getPrefectureId() != null && form.getPrefectureId() != 0) {
+
+					//所在地を条件に追加
+					Predicate newCondition = cb.equal(root.get("prefectureId"), form.getPrefectureId());
+					condition = getPredicate(cb, condition, newCondition);
+				}
+
+				if(form.getTenshuId() != null && form.getTenshuId() != 0) {
+
+					//天守を条件に追加
+					Predicate newCondition = cb.equal(root.get("tenshuId"), form.getTenshuId());
+					condition = getPredicate(cb, condition, newCondition);
+				}
+
+				//削除フラグを条件に追加
+				Predicate newCondition = cb.equal(root.get("delFlg"), CommonConst.DELETE_FLG_OFF);
+				condition = getPredicate(cb, condition, newCondition);
+				return condition;
+			}
+
+			/**
+			 * 検索条件を結合する
+			 *
+			 * @param cb
+			 * @param condition
+			 * @param newCondition
+			 * @return Predicate
+			 */
+			private Predicate getPredicate(CriteriaBuilder cb, Predicate condition, @NotNull Predicate newCondition) {
+				if(condition != null) {
+
+					//すでに条件がある場合ANDで結合する
+					condition = cb.and(condition, newCondition);
+				}else {
+
+					//条件がまだない場合先頭の条件になる
+					condition = newCondition;
+				}
+				return condition;
+			}
+		};
+	}
+}
