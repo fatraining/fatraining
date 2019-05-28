38847cbd5785fa1d16e6bd4ffbbab69a4cd25bdf
fatraining <40687688+fatraining@users.noreply.github.com>
2019-05-28 15:54:16 +0900

    温泉のServiceです。

---
+++ web-app-training/src/main/java/jp/co/futureantiques/webapptraining/service/SpaOkabeService.java
@@ -0,0 +1,70 @@
+package jp.co.futureantiques.webapptraining.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import jp.co.futureantiques.webapptraining.model.form.spaOkabe.SpaOkabeSearchForm;
+import jp.co.futureantiques.webapptraining.model.spaOkabe.AreaOkabe;
+import jp.co.futureantiques.webapptraining.model.spaOkabe.QualityOkabe;
+import jp.co.futureantiques.webapptraining.model.spaOkabe.SpaMainOkabe;
+
+/**
+ *SpaOkabeのサービスインターフェース
+ *
+ * @author SEIYA OKABE
+ */
+
+public interface SpaOkabeService {
+
+	/**
+	 * 都道府県エンティティのリストを取得する
+	 *
+	 * @return AreaOkabeEntityのリスト
+	 */
+	List<AreaOkabe> getListAreaOkabe();
+
+	/**
+	 * 泉質エンティティのリストを取得する
+	 *
+	 * @return QualityOkabeEntityのリスト
+	 */
+	List<QualityOkabe> getListQualityOkabe();
+
+	/**
+	 * 検索条件を元にSpaMainのレコードのリストを取得する(Paging)
+	 *
+	 * @param SpaOkabeSearchForm form
+	 * @param Pageable pageable
+	 * @return SpaMainOkabeのリスト
+	 */
+	Page<SpaMainOkabe> getPageSpa(final SpaOkabeSearchForm form, final Pageable pageable);
+
+	/**
+	 * 検索条件を元にSpaMainOkabeのレコードのリストを取得する
+	 *
+	 * @param SpaOkabeSearchForm form
+	 * @return SpaMainOkabeのリスト
+	 */
+	List<SpaMainOkabe> getListSpaMainOkabe(final SpaOkabeSearchForm form);
+
+	/**
+	 * IDをキーにSpaMainOkabeのレコードを取得する
+	 *
+	 * @param long id
+	 * @return SpaMainOkabeのレコード
+	 */
+	SpaMainOkabe getSpaMainOkabe(final long id);
+
+
+	/**
+	 * SpaMainOkabeのレコードを論理削除する
+	 *
+	 * @param long id
+	 */
+	void deleteSpaById(final long id);
+
+
+
+}
