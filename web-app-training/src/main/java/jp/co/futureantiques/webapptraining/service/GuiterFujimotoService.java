300c882f42162b57ed879d9b1d20d50d14ec82d3
fatraining <40687688+fatraining@users.noreply.github.com>
2019-05-29 16:14:23 +0900

    ギターサービスをコミットしました。（藤本）

---
+++ web-app-training/src/main/java/jp/co/futureantiques/webapptraining/service/GuiterFujimotoService.java
@@ -0,0 +1,71 @@
+package jp.co.futureantiques.webapptraining.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import jp.co.futureantiques.webapptraining.model.GuiterFujimoto.CompanyFujimoto;
+import jp.co.futureantiques.webapptraining.model.GuiterFujimoto.GuiterMainFujimoto;
+import jp.co.futureantiques.webapptraining.model.GuiterFujimoto.WoodFujimoto;
+import jp.co.futureantiques.webapptraining.model.form.guiterFujimoto.GuiterSearchForm;
+
+/*import jp.co.futureantiques.webapptraining.model.IcecreamSako.GenreSako;
+import jp.co.futureantiques.webapptraining.model.IcecreamSako.IcecreamMainSako;
+import jp.co.futureantiques.webapptraining.model.IcecreamSako.MakerSako;
+import jp.co.futureantiques.webapptraining.model.form.icecreamSako.IcecreamSearchForm;*/
+
+/**
+ * IcecreamSakoのサービスインターフェース
+ *
+ * @author MINAMI SAKO
+ */
+@Service
+public interface GuiterFujimotoService {
+
+	/**
+	 * 会社エンティティのリストを取得する
+	 *
+	 * @return CompanyFujimotoEntityのリスト
+	 */
+	List<CompanyFujimoto> getListCompanyFujimoto();
+
+	/**
+	 * 木材のリストを取得する
+	 *
+	 * @return WoodFujimotoEntityのリスト
+	 */
+	List<WoodFujimoto> getListWoodFujimoto();
+
+	/**
+	 * 検索条件を元にGuiterMainFujimotoのレコードのリストを取得する(Paging)
+	 *
+	 * @param guiterSearchForm form
+	 * @param Pageable pageable
+	 * @return IcecreamMainSakoのリスト
+	 */
+	Page<GuiterMainFujimoto> getPageGuiter(final GuiterSearchForm form, final Pageable pageable);
+
+	/**
+	 * 検索条件を元にGuiterMainFujimotoのレコードのリストを取得する
+	 *
+	 * @param GuiterSearchForm form
+	 * @return GuiterMainFujimotoのリスト
+	 */
+	List<GuiterMainFujimoto> getListGuiter(final GuiterSearchForm form);
+
+	/**
+	 * IDをキーにGuiterMainFujimotoのレコードを取得する
+	 *
+	 * @param long id
+	 * @return GuiterMainFujimotoのレコード
+	 */
+	GuiterMainFujimoto getGuiter(final long id);
+
+/**論理削除
+ *
+ */
+
+//void deleteGuiterById(final long id);
+}
