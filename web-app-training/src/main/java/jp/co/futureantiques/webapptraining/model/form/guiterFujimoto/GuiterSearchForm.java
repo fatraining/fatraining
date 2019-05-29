714f96cf8e3a3922cd246b1f2993eb583881f47d
fatraining <40687688+fatraining@users.noreply.github.com>
2019-05-29 16:13:12 +0900

    ギターsearchフォームをコミットしました。（藤本）

---
+++ web-app-training/src/main/java/jp/co/futureantiques/webapptraining/model/form/guiterFujimoto/GuiterSearchForm.java
@@ -0,0 +1,41 @@
+package jp.co.futureantiques.webapptraining.model.form.guiterFujimoto;
+
+import jp.co.futureantiques.webapptraining.model.GuiterFujimoto.GuiterMainFujimoto;
+import lombok.Data;
+
+/**
+ * 検索画面のFormクラス
+ *
+ * @author FUJIMOTO　YOSIHISA
+ *
+ */
+@Data
+
+public class GuiterSearchForm {
+	/**ID*/
+	private Integer id;
+
+	/**名前*/
+	private String name;
+
+	/**会社ID*/
+	private Integer companyId;
+
+	/**木材Id*/
+	private Integer woodId;
+
+	/** 削除フラグON用*/
+	private int isDelete;
+
+	/**
+	 * 各フィールドに値を設定する
+	 *
+	 * @param guiterMainFujimoto
+	 */
+	public void setValues(GuiterMainFujimoto guiterMainFujimoto){
+		this.setId((int)guiterMainFujimoto.getId());
+		this.setName(guiterMainFujimoto.getAconame());
+		this.setCompanyId(guiterMainFujimoto.getCompanyId());
+		this.setWoodId(guiterMainFujimoto.getWoodId());
+	}
+}
