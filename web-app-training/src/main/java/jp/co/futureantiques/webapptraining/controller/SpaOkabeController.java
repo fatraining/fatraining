260e562b7ad707f438221136fa3937011fb02e66
fatraining <40687688+fatraining@users.noreply.github.com>
2019-06-03 15:21:40 +0900

    温泉のControllerに追記しました。　岡部

--- web-app-training/src/main/java/jp/co/futureantiques/webapptraining/controller/SpaOkabeController.java
+++ web-app-training/src/main/java/jp/co/futureantiques/webapptraining/controller/SpaOkabeController.java
@@ -7,11 +7,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jp.co.futureantiques.webapptraining.constant.CommonConst;
+import jp.co.futureantiques.webapptraining.model.form.spaOkabe.SpaOkabeDeleteForm;
+import jp.co.futureantiques.webapptraining.model.form.spaOkabe.SpaOkabeInputForm;
 import jp.co.futureantiques.webapptraining.model.form.spaOkabe.SpaOkabeSearchForm;
 import jp.co.futureantiques.webapptraining.model.spaOkabe.AreaOkabe;
 import jp.co.futureantiques.webapptraining.model.spaOkabe.QualityOkabe;
@@ -95,6 +100,84 @@ public class SpaOkabeController {
 		return "spaOkabe/search";
 
 	}
+
+	/**
+	 * 追加画面に遷移する
+	 *
+	 * @param SpaOkabeInputForm spaOkabeInputForm
+	 * @return 追加画面のパス
+	 */
+	@RequestMapping(value = "insert", method = RequestMethod.GET)
+	public String showInsertSpa(@ModelAttribute final SpaOkabeInputForm spaOkabeInputForm) {
+		return "spaOkabe/insert";
+	}
+
+	/**
+	 * SpaMainOkabeテーブルにデータを登録して検索画面に遷移する
+	 *
+	 * @param SpaOkabeInputForm form
+	 * @param BindingResult bindingResult
+	 * @return 入力エラーがある場合追加画面、ない場合検索画面のパス
+	 */
+	@RequestMapping(value = "insert", method = RequestMethod.POST)
+	public String insertSpa(@ModelAttribute @Validated final SpaOkabeInputForm form,
+			final BindingResult bindingResult) {
+		if (bindingResult.hasFieldErrors()) {
+
+			// 入力エラーがある場合自画面に戻る
+			return "spaOkabe/insert";
+		}
+
+		// データを登録する
+		final SpaMainOkabe spaMainOkabe = spaOkabeService.insertSpa(form);
+		return "redirect:/spaOkabe?result=insert&id=" + spaMainOkabe.getId();
+	}
+
+	/**
+	 * 更新画面に遷移する
+	 *
+	 * @param long id
+	 * @param SpaOkabeInputForm movieSampleInputForm
+	 * @return 更新画面のパス
+	 */
+	@RequestMapping(value = "update", method = RequestMethod.GET)
+	public String showUpdateSpa(@RequestParam(name = "id") final long id,
+			@ModelAttribute final SpaOkabeInputForm spaOkabeInputForm) {
+
+		// IDをキーにMovieMainテーブルを検索する
+		SpaMainOkabe spaMainOkabe = spaOkabeService.getSpaMainOkabe(id);
+
+		// フォームにレコードの値をセットする
+		spaOkabeInputForm.initWithSpaMainOkabe(spaMainOkabe);
+		return "spaOkabe/update";
+	}
+
+	/**
+	 * SpaMainOkabeテーブルのデータを更新して検索画面に遷移する
+	 *
+	 * @param SpaOkabeInputForm form
+	 * @param BindingResult bindingResult
+	 * @return 入力エラーがある場合更新画面、ない場合検索画面のパス
+	 */
+	@RequestMapping(value = "update", method = RequestMethod.POST)
+	public String updateSpa(@Validated final SpaOkabeInputForm form,
+			final BindingResult bindingResult) {
+		if (bindingResult.hasFieldErrors()) {
+
+			// 入力エラーがある場合自画面に戻る
+			return "spaOkabe/update";
+		}
+
+		// データを更新する
+		SpaMainOkabe spaMainOkabe = spaOkabeService.updateSpa(form);
+		if (spaMainOkabe == null) {
+
+			// 更新が失敗した場合、検索画面にメッセージを表示する
+			return "redirect:/spaOkabe?result=updatefailed";
+		}
+		return "redirect:/spaOkabe?result=update&id=" + spaMainOkabe.getId();
+	}
+
 	/**
 	 * SpaMainOkabeテーブルのレコードを論理削除して検索画面に遷移する
 	 *
@@ -106,6 +189,52 @@ public class SpaOkabeController {
 
 		//IDをキーにレコードを論理削除する
 		spaOkabeService.deleteSpaById(id);
-		return "redirect:/spa?result=delete&id" + id;
+		return "redirect:/spaOkabe?result=delete&id" + id;
+	}
+	/**
+	 * 完全削除画面に遷移する
+	 *
+	 * @param SpaOkabeSearchForm form
+	 * @param SpaOkabeDeleteForm spaOkabeDeleteForm
+	 * @param Model model
+	 * @return 完全削除画面のパス
+	 */
+	@RequestMapping(value = "deletecomp", method = RequestMethod.GET)
+	public String showDeleteCompSpa(final SpaOkabeSearchForm form,
+			@ModelAttribute final SpaOkabeDeleteForm spaOkabeDeleteForm, final Model model) {
+
+		// MovieMainテーブルから削除フラグが1のレコードを検索する
+		final List<SpaMainOkabe> spaOkabeList = spaOkabeService.getListSpaMainOkabe(form);
+
+		// Modelに検索結果を格納する
+		model.addAttribute(spaOkabeList);
+		return "spaOkabe/deletecomp";
+	}
+	/**
+	 * SpaMainOkabeテーブルのデータを完全削除して検索画面に遷移する
+	 *
+	 * @param SpaOkabeDeleteForm form
+	 * @param BindingResult bindingResult
+	 * @param Model model
+	 * @return 入力エラーがある場合完全削除画面、ない場合検索画面のパス
+	 */
+	@RequestMapping(value = "deletecomp", method = RequestMethod.POST)
+	public String deleteCompSpa(@Validated final SpaOkabeDeleteForm form,
+			final BindingResult bindingResult, final Model model) {
+		if (bindingResult.hasFieldErrors()) {
+
+			// 入力エラーがある場合、再検索して自画面に戻る
+			SpaOkabeSearchForm spaOkabeSearchForm = new SpaOkabeSearchForm();
+			spaOkabeSearchForm.setIsDelete(CommonConst.DELETE_FLG_ON);
+			final List<SpaMainOkabe> spaOkabeList = spaOkabeService.getListSpaMainOkabe(spaOkabeSearchForm);
+
+			// Modelに検索結果を格納する
+			model.addAttribute(spaOkabeList);
+			return "spaOkabe/deletecomp";
+		}
+
+		// データを完全削除する
+		spaOkabeService.deleteSpaComp(form.getDeleteIds());
+		return "redirect:/spaOkabe?result=deletecomp";
 	}
 }
