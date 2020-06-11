ecd95ac709581369f3bb92b8461106ed8daadba5
fatraining <40687688+fatraining@users.noreply.github.com>
2020-06-11 13:26:05 +0900

    漫画検索機能新規追加　三好

---
+++ web-app-training/src/main/java/jp/co/futureantiques/webapptraining/model/form/comicMiyoshi/ComicMiyoshiSearchForm.java
@@ -0,0 +1,39 @@
+package jp.co.futureantiques.webapptraining.model.form.comicMiyoshi;
+
+import jp.co.futureantiques.webapptraining.model.comicMiyoshi.ComicMainMiyoshi;
+import lombok.Data;
+
+/**
+ * 漫画検索画面のFormクラス
+ * @author Miyoshi
+ */
+@Data
+public class ComicMiyoshiSearchForm {
+
+	/** 漫画ID */
+	private Integer id;
+
+	/** 漫画タイトル */
+	private String comicTitle;
+
+	/** 原作者名 */
+	private String author;
+
+	/** 連載雑誌名 */
+	private Integer magazineId;
+
+	/** 削除フラグON用 */
+	private int isDelete;
+
+	/**
+	 * 各フィールドに値を設定する
+	 * @param comicMainMiyoshi
+	 */
+
+	public void setValue(ComicMainMiyoshi comicMainMiyoshi) {
+		this.setId((int) comicMainMiyoshi.getId());
+		this.setComicTitle(comicMainMiyoshi.getComicTitle());
+		this.setAuthor(comicMainMiyoshi.getAuthor());
+		this.setMagazineId(comicMainMiyoshi.getMagazineId());
+	}
+}
---
+++ web-app-training/src/main/java/jp/co/futureantiques/webapptraining/repository/comicMiyoshi/MagazineMiyoshiRepository.java
@@ -0,0 +1,13 @@
+package jp.co.futureantiques.webapptraining.repository.comicMiyoshi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jp.co.futureantiques.webapptraining.model.comicMiyoshi.MagazineMiyoshi;
+
+/**
+ * MagazineMiyoshiリポジトリのインターフェース
+ * @author Miyoshi
+ */
+public interface MagazineMiyoshiRepository extends JpaRepository<MagazineMiyoshi, Long>{
+
+}
