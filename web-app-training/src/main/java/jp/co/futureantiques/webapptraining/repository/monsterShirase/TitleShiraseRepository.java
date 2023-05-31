e5034316f477b36261cd8cf3b089446769cfd693
fa <fa@192.168.40.30>
2023-05-31 09:08:49 +0900

    TitleとMonsterMainリポジトリの追加忘れ 白勢裕一朗

---
+++ web-app-training/src/main/java/jp/co/futureantiques/webapptraining/repository/monsterShirase/TitleShiraseRepository.java
@@ -0,0 +1,13 @@
+package jp.co.futureantiques.webapptraining.repository.monsterShirase;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jp.co.futureantiques.webapptraining.model.monsterShirase.TitleShirase;
+
+/**
+ * TitleShiraseリポジトリのインターフェース
+ * @author Shirase
+ *
+ */
+public interface TitleShiraseRepository extends JpaRepository<TitleShirase, Long> {
+}
